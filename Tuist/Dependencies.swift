3998f2b7cbee8d1e9b552eca3c8414fbf5175dbd
baegteun <baegteun@gmail.com>
2022-09-18 13:32:13 +0900

    :heavy_plus_sign: :: FeatureThirdPartyLib, NeedleFoundation

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,7 +4,8 @@ let dependencies = Dependencies(
     carthage: nil,
     swiftPackageManager: [
         .remote(url: "https://github.com/Quick/Quick.git", requirement: .upToNextMajor(from: "5.0.1")),
-        .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0"))
+        .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0")),
+        .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0"))
     ],
     platforms: [.iOS]
 )
