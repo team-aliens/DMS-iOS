c7f54ef78f1b29a78d7bb94b1d3ad66b18901ec6
baegteun <baegteun@gmail.com>
2022-09-21 00:26:49 +0900

    refactor :: basedOnDependency false RunScripting

--- Tuist/ProjectDescriptionHelpers/Action+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Action+Template.swift
@@ -3,11 +3,13 @@ import ProjectDescription
 public extension TargetScript {
     static let swiftLint = TargetScript.pre(
         path: Path.relativeToRoot("Scripts/SwiftLintRunScript.sh"),
-        name: "SwiftLint"
+        name: "SwiftLint",
+        basedOnDependencyAnalysis: false
     )
 
     static let needle = TargetScript.pre(
         path: .relativeToRoot("Scripts/NeedleRunScript.sh"),
-        name: "Needle"
+        name: "Needle",
+        basedOnDependencyAnalysis: false
     )
 }
