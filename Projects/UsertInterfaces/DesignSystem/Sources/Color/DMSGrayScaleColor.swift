7245078d038ae1f277e309265faedd2811190e85
sian7563 <sian7563@naver.com>
2022-09-25 23:22:16 +0900

    feat: DMSGrayScaleColor

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Color/DMSGrayScaleColor.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+public extension Color {
+    struct GrayScaleColor { }
+}
+
+public extension Color.GrayScaleColor {
+    static let gray1: Color = Color("Gray 1")
+    static let gray2: Color = Color("Gray 2")
+    static let gray3: Color = Color("Gray 3")
+    static let gray4: Color = Color("Gray 4")
+    static let gray5: Color = Color("Gray 5")
+    static let gray6: Color = Color("Gray 6")
+    static let gray7: Color = Color("Gray 7")
+    static let gray8: Color = Color("Gray 8")
+    static let gray9: Color = Color("Gray 9")
+}
