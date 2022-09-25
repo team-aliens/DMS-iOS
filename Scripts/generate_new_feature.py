0f40ae5925c21e1b438dadae78f8182eb5624803
baegteun <baegteun@gmail.com>
2022-09-25 13:50:24 +0900

    feat :: dependency에 자동으로 feature 경로 추가

--- Scripts/generate_new_feature.py
+++ Scripts/generate_new_feature.py
@@ -30,8 +30,7 @@ def make_dirs(paths):
 def make_project_file(feature_name, file_path, has_demo=False, dependencies=[]):
     project_path = file_path + '/Project.swift'
     file_name = file_path.split('/')[-1]
-    file_content = f"""
-import ProjectDescription
+    file_content = f"""import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
@@ -131,7 +130,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 '''
     write_code_in_file(app_delegate_path, app_delegate)
 
+def write_created_feature_on_dependency_project(feature_name):
+    file_path = f'{root_path}/Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+Project.swift'
+    read_file = read_file_at(file_path)
+    updated_file = update_file_at(read_file, feature_name)
+    write_file_at(file_path, updated_file)
 
+def read_file_at(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+def update_file_at(dependency_file, feature_name):
+    dependency_file_len = len(dependency_file)
+    for index, elem in enumerate(dependency_file):
+        if "public extension TargetDependency.Project.Features" in elem:
+            insert_line = index + 1
+            break
+        
+    append_code = f'    static let {feature_name}Feature = TargetDependency.feature(name: "{feature_name}Feature")\n'
+
+    if dependency_file_len > int(insert_line):
+        dependency_file.insert(insert_line, append_code)
+    
+    return dependency_file
+
+def write_file_at(file_path, update_file):
+    with open(file_path, 'w') as file:
+        file.writelines(update_file)
 
 
 print('Input new feature name ', end=': ', flush=True)
@@ -149,3 +174,6 @@ root_path = os.getcwd()
 os.chdir(root_path + '/Projects/Features')
 
 make_new_feature(feature_name, has_demo)
+write_created_feature_on_dependency_project(feature_name)
+
+print(f'Created {feature_name}Feature Module')
