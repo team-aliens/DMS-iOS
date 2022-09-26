408f52bf1fb27179df010bb06165611e6f22413b
sian7563 <sian7563@naver.com>
2022-09-26 18:26:49 +0900

    feat :: DMSPrimaryColor 수정

--- Projects/UsertInterfaces/DesignSystem/Sources/Color/DMSPrimaryColor.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Color/DMSPrimaryColor.swift
@@ -5,9 +5,9 @@ public extension Color {
 }
 
 public extension Color.PrimaryVariant {
-    static let lighten1: Color = Color("Lighten-1")
-    static let lighten2: Color = Color("Lighten-2")
-    static let primary: Color = Color("Primary")
-    static let darken1: Color = Color("Darken-1")
-    static let darken2: Color = Color("Darken-2")
+    static let lighten1: Color = DesignSystemAsset.PrimaryColor.lighten1.suiColor
+    static let lighten2: Color = DesignSystemAsset.PrimaryColor.lighten2.suiColor
+    static let primary: Color = DesignSystemAsset.PrimaryColor.primary.suiColor
+    static let darken1: Color = DesignSystemAsset.PrimaryColor.darken1.suiColor
+    static let darken2: Color = DesignSystemAsset.PrimaryColor.darken2.suiColor
 }
