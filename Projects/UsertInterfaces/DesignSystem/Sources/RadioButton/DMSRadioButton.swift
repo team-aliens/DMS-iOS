4dc636afaad6bad2e6314fc186942acafb2255a3
sian7563 <sian7563@naver.com>
2022-09-29 17:18:43 +0900

    feat :: RadioButton 수정

--- Projects/UsertInterfaces/DesignSystem/Sources/RadioButton/DMSRadioButton.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/RadioButton/DMSRadioButton.swift
@@ -5,28 +5,17 @@ public struct DMSRadioButton: View {
     @Binding var isOn: Bool
     @Binding var isDisabeld: Bool
 
-    let id: String
-    let size: CGFloat
-    let callback: (String) -> Void
-
     public init(
         isOn: Binding<Bool> = .constant(false),
-        isDisabled: Binding<Bool> = .constant(false),
-        id: String,
-        size: CGFloat = 20,
-        callback: @escaping (String) -> Void
+        isDisabled: Binding<Bool> = .constant(false)
     ) {
         self._isOn = isOn
         self._isDisabeld = isDisabled
-        self.id = id
-        self.size = size
-        self.callback = callback
     }
 
     public var body: some View {
         Button {
             self.isOn.toggle()
-            self.callback(self.id)
         } label: {
             HStack(alignment: .center) {
                 if self.isDisabeld {
@@ -45,9 +34,6 @@ public struct DMSRadioButton: View {
 
 struct DMSRadioButton_Previews: PreviewProvider {
     static var previews: some View {
-        DMSRadioButton(
-            id: "asdf",
-            callback: { _ in }
-        )
+        DMSRadioButton()
     }
 }
