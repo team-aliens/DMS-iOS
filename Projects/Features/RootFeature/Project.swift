9c716339bee959a721b446f413945f008a9ba852
baegteun <baegteun@gmail.com>
2022-10-12 19:37:44 +0900

    build :: CommonFeature의 이름을 BaseFeature로 변경

--- Projects/Features/RootFeature/Project.swift
+++ Projects/Features/RootFeature/Project.swift
@@ -5,6 +5,6 @@ let project = Project.makeModule(
     name: "RootFeature",
     product: .staticFramework,
     dependencies: [
-        .Project.Features.CommonFeature
+        .Project.Features.BaseFeature
     ]
 )
