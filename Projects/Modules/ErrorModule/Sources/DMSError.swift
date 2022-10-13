30dc531c98d313401e11d0a344297bb9df580766
baegteun <baegteun@gmail.com>
2022-10-13 15:52:17 +0900

    feat :: BaseViewModel

--- Projects/Modules/ErrorModule/Sources/DMSError.swift
+++ Projects/Modules/ErrorModule/Sources/DMSError.swift
@@ -5,6 +5,18 @@ public enum DMSError: Error {
     case custom(message: String, code: Int)
 }
 
+extension DMSError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "알 수 없는 오류가 발생하였습니다"
+
+        case let .custom(message, _):
+            return message
+        }
+    }
+}
+
 public extension Error {
     var asDMSError: DMSError {
         self as? DMSError ?? .unknown
