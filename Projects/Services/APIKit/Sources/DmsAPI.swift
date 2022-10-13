88e3159b7d81a4f96fcba58a5d5f507c9752e4ec
baegteun <baegteun@gmail.com>
2022-10-13 16:13:10 +0900

    feat :: DmsAPI

--- Projects/Services/APIKit/Sources/DmsAPI.swift
+++ Projects/Services/APIKit/Sources/DmsAPI.swift
@@ -1,6 +1,38 @@
 import Foundation
 import Moya
+import ErrorModule
 
-public protocol DmsAPI: TargetType {
-    
+public protocol DmsAPI: TargetType, JwtAuthorizable {
+    var domain: DmsDomain { get }
+    var urlPath: String { get }
+    var errorMap: [Int: DmsError] { get }
+}
+
+public extension DmsAPI {
+    var baseURL: URL {
+        URL(string: "https://google.com")!
+    }
+
+    var path: String {
+        domain.asURLString + urlPath
+    }
+
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+}
+
+public enum DmsDomain: String {
+    case auth
+    case users
+    case losts
+    case notices
+    case meal
+    case images
+}
+
+extension DmsDomain {
+    var asURLString: String {
+        "/\(self.rawValue)"
+    }
 }
