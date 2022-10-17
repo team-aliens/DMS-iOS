5d675960e2843c06f1e9d887da04f72eb33d22ad
baegteun <baegteun@gmail.com>
2022-10-17 13:57:04 +0900

    feat :: Auth Domain Service DI

--- Projects/App/Sources/Application/DI/Auth/AppComponent+Auth.swift
+++ Projects/App/Sources/Application/DI/Auth/AppComponent+Auth.swift
@@ -5,7 +5,7 @@ import DataModule
 
 public extension AppComponent {
     var remoteAuthDataSource: any RemoteAuthDataSource {
-        RemoteAuthDataSourceStub()
+        RemoteAuthDataSourceStub() // TODO: 퍼블리싱용 Stub, 테스트 서버 나오면 변경
 //        RemoteAuthDataSourceImpl(keychain: keychain)
     }
 
