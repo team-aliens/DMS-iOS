8af01b7a23919d99cb38e43150f7c8c39c55b044
baegteun <baegteun@gmail.com>
2022-10-17 14:21:08 +0900

    refactor :: 린트 맞추기

--- Projects/Services/DataMappingModule/Sources/Auth/Request/SigninRequestDTO.swift
+++ Projects/Services/DataMappingModule/Sources/Auth/Request/SigninRequestDTO.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 public struct SigninRequestDTO: Encodable {
+    public let accountID: String
+    public let password: String
+
     public init(accountID: String, password: String) {
         self.accountID = accountID
         self.password = password
     }
-
-    public let accountID: String
-    public let password: String
 
     enum CodingKeys: String, CodingKey {
         case accountID = "account_id"
--- Projects/Services/DataMappingModule/Sources/Auth/Response/CheckAccountIDIsExistResponseDTO.swift
+++ Projects/Services/DataMappingModule/Sources/Auth/Response/CheckAccountIDIsExistResponseDTO.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 public struct CheckAccountIDIsExistResponseDTO: Decodable {
+    public let email: String
+
     public init(email: String) {
         self.email = email
     }
-
-    public let email: String
 }
