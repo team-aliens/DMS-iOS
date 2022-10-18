1481091d76152191418bcb01092fcfa10d9721a2
baegteun <baegteun@gmail.com>
2022-10-18 09:38:22 +0900

    feat :: Students Service DI

---
+++ Projects/App/Sources/Application/DI/Students/AppComponent+Students.swift
@@ -0,0 +1,38 @@
+import NeedleFoundation
+import NetworkModule
+import DomainModule
+import DataModule
+
+public extension AppComponent {
+    var remoteStudentsDataSource: any RemoteStudentsDataSource {
+        RemoteStudentsDataSourceStub()
+    }
+
+    var studentsRepository: any StudentsRepository {
+        StudentsRepositoryImpl(remoteStudentsDataSource: remoteStudentsDataSource)
+    }
+
+    var signupUseCase: any SignupUseCase {
+        SignupUseCaseImpl(studentsRepository: studentsRepository)
+    }
+
+    var checkDuplicateAccountIDUseCase: any CheckDuplicateAccountIDUseCase {
+        CheckDuplicateAccountIDUseCaseImpl(studentsRepository: studentsRepository)
+    }
+
+    var checkDuplicateEmailUseCase: any CheckDuplicateEmailUseCase {
+        CheckDuplicateEmailUseCaseImpl(studentsRepository: studentsRepository)
+    }
+
+    var renewalPasswordUseCase: any RenewalPasswordUseCase {
+        RenewalPasswordUseCaseImpl(studentsRepository: studentsRepository)
+    }
+
+    var findIDUseCase: any FindIDUseCase {
+        FindIDUseCaseImpl(studentsRepository: studentsRepository)
+    }
+
+    var fetchMyProfileUseCase: any FetchMyProfileUseCase {
+        FetchMyProfileUseCaseImpl(studentsRepository: studentsRepository)
+    }
+}
