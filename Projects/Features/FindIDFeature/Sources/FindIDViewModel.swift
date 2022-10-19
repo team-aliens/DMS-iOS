f522b366b64e78e479eedfda7284991b5d0df257
Daehee Kim <0824dh@naver.com>
2022-10-19 17:02:30 +0900

    feat :: findIdViewModel에 임시 학교값 주입

--- Projects/Features/FindIDFeature/Sources/FindIDViewModel.swift
+++ Projects/Features/FindIDFeature/Sources/FindIDViewModel.swift
@@ -6,10 +6,17 @@ final class FIndIDViewModel: BaseViewModel {
 
     @Published var schoolID = ""
     @Published var name = ""
-    @Published var grade = 0
-    @Published var classRoom = 0
-    @Published var number = 0
+    @Published var grade = ""
+    @Published var classRoom = ""
+    @Published var number = ""
 
+    @Published var schoolList =  [
+        "대덕소프트웨어마이스터고등학교",
+        "대구소프트웨어마이스터고등학교",
+        "광주소프트웨어마이스터고등학교",
+        "부산소프트웨어마이스터고등학교",
+        "미림여자정보과학고"
+    ]
     @Published var isNavigateSignup = false
 
     private let findIDUseCase: any FindIDUseCase
