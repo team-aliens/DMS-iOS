e77960bf753b15cf79226c292b81041934f22fd8
Daehee Kim <0824dh@naver.com>
2022-10-19 21:48:30 +0900

    feat :: FindIdView scene

--- Projects/Features/FindIDFeature/Sources/FindIDView.swift
+++ Projects/Features/FindIDFeature/Sources/FindIDView.swift
@@ -1,23 +1,75 @@
 import SwiftUI
 import DesignSystem
 
-public struct FindIdView: View {
+struct FindIdView: View {
 
+    @FocusState private var focusField: FocusField?
     @StateObject var viewModel: FIndIDViewModel
 
-    public var body: some View {
+    private enum FocusField {
+        case grade
+        case classRoom
+        case number
+        case name
+    }
+
+    public init(viewModel: FIndIDViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
         VStack {
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
                     Text("DMS")
                         .dmsFont(.title(.extraLarge), color: .PrimaryVariant.primary)
                         .padding(.top, 28)
-
-                    Text("더 편한 기숙사 생활을 위해")
+                    Text("아이디 찾기")
                         .dmsFont(.text(.medium), color: .GrayScale.gray6)
                 }
+                .padding(.top, 28)
                 Spacer()
             }
+            Spacer()
+                .frame(height: 70)
+            VStack(spacing: 60) {
+                SchoolSelectButtonView(schoolList: $viewModel.schoolList)
+
+                HStack(spacing: 20) {
+                    DMSFloatingTextField("학년", text: $viewModel.grade) {
+                        focusField = .classRoom
+                    }
+                    .focused($focusField, equals: .grade)
+
+                    DMSFloatingTextField("반", text: $viewModel.classRoom) {
+                        focusField = .number
+                    }
+                    .focused($focusField, equals: .classRoom)
+
+                    DMSFloatingTextField("번호", text: $viewModel.number) {
+                        focusField = .name
+                    }
+                    .focused($focusField, equals: .number)
+
+                }
+                DMSFloatingTextField("이름", text: $viewModel.name) {
+                    viewModel.findIDButtonDidTap()
+                }
+                .focused($focusField, equals: .name)
+
+            }
+
+            Spacer()
+                .padding(.top, 68)
+
+            DMSWideButton(text: "아이디 찾기", color: .PrimaryVariant.primary) {
+                viewModel.findIDButtonDidTap()
+            }
+
         }
+        .navigationTitle("아이디 찾기")
+        .dmsBackground()
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(24)
     }
 }
