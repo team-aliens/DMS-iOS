6adadc202eb80c2fae869e7a2012d6346e9facd6
Daehee Kim <0824dh@naver.com>
2022-10-20 01:32:07 +0900

    feat :: SchoolAPI

--- Projects/Services/APIKit/Sources/SchoolAPI.swift
+++ Projects/Services/APIKit/Sources/SchoolAPI.swift
@@ -5,7 +5,84 @@ import Foundation
 
 public enum SchoolAPI {
     case viewSchoolList
-    case viewVerificationQuestion
-    case checkVerificationQuestion
-    case inspectionSchoolCertificationCode
+    case getSchoolQuestion(authCode: String)
+    case checkSchoolQuestion(schoolID: String, answer: String)
+    case checkSchoolAuthCode(schoolID: String, schoolCode: String)
+}
+
+extension SchoolAPI: DmsAPI {
+    public var domain: DmsDomain {
+        .schools
+     }
+    public var urlPath: String {
+        switch self {
+        case .viewSchoolList:
+            return "/"
+        case let .getSchoolQuestion(authCode):
+            return "/question/\(authCode)"
+        case let .checkSchoolQuestion(schoolID, _):
+            return "/answer/\(schoolID)"
+        case let .checkSchoolAuthCode(schoolID, _):
+            return "/code/\(schoolID)"
+        }
+    }
+
+    public var errorMap: [Int: ErrorModule.DmsError] {
+        switch self {
+        case .viewSchoolList:
+            return [
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+        case .getSchoolQuestion:
+            return [
+                400: .badRequest,
+                404: .notFoundSchool,
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+
+        case .checkSchoolQuestion:
+            return [
+                400: .badRequest,
+                401: .invalidQuestionAnswer,
+                404: .notFoundSchool,
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+
+        case .checkSchoolAuthCode:
+            return [
+                400: .badRequest,
+                401: .invalidQuestionAnswer,
+                404: .notFoundSchool,
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+        }
+    }
+
+    public var method: Moya.Method {
+        return .get
+    }
+
+    public var task: Moya.Task {
+        switch self {
+        case let .checkSchoolQuestion(_, answer):
+            return .requestParameters(parameters: [
+                "answer": answer
+            ], encoding: URLEncoding.queryString)
+        case let .checkSchoolAuthCode(_, schoolCode):
+            return .requestParameters(parameters: [
+                "school_code": schoolCode
+            ], encoding: URLEncoding.queryString)
+        default:
+            return .requestPlain
+        }
+    }
+
+    public var jwtTokenType: JwtTokenType {
+        return .none
+    }
+
 }
