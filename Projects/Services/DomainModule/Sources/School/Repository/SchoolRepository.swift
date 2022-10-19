37f7129d47e951bb053135049a176fc651b86d78
Daehee Kim <0824dh@naver.com>
2022-10-20 08:33:41 +0900

    feat :: SchoolRepository

---
+++ Projects/Services/DomainModule/Sources/School/Repository/SchoolRepository.swift
@@ -0,0 +1,10 @@
+import Combine
+import DataMappingModule
+import ErrorModule
+
+public protocol SchoolRepository {
+    func getSchoolList() -> AnyPublisher<FetchSchoolListResponseDTO, DmsError>
+    func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionResponseDTO, DmsError>
+    func checkSchoolQuestion(schoolID: String, answer: String) -> AnyPublisher<Void, DmsError>
+    func checkSchoolAuthCode(schoolID: String, schoolCode: String) -> AnyPublisher<CheckSchoolAuthCodeResponseDTO, DmsError>
+}
