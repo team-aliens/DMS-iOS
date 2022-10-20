533c592313205323e6185c224096c3468723b66c
baegteun <baegteun@gmail.com>
2022-10-20 20:36:39 +0900

    delete :: Unused Entity

--- Projects/Services/DomainModule/Sources/Entities/CheckSchoolQuestionEntity.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct CheckSchoolQuestionEntity: Equatable {
-    public let question: String
-
-    public init(question: String) {
-        self.question = question
-    }
-}
