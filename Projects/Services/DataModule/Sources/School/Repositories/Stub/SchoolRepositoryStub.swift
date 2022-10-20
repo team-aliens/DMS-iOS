1aeca596317ed127ec04b6e7d884518920d2197c
Daehee Kim <0824dh@naver.com>
2022-10-20 16:20:40 +0900

    feat :: SchoolRepositoryStub

--- Projects/Services/DataModule/Sources/School/Repositories/Stub/SchoolRepositoryStub.swift
+++ Projects/Services/DataModule/Sources/School/Repositories/Stub/SchoolRepositoryStub.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2022 com.team.aliens. All rights reserved.
 //
 
-import Foundation
+import Combine
+import DataMappingModule
+import DomainModule
+import ErrorModule
+import NetworkModule
+
+public struct SchoolRepositoryStub: SchoolRepository {
+    private let remoteSchoolDataSource: any RemoteSchoolDataSource
+
+    public init(remoteSchoolDataSource: any RemoteSchoolDataSource) {
+        self.remoteSchoolDataSource = remoteSchoolDataSource
+    }
+
+    public func getSchoolList() -> AnyPublisher<[SchoolEntity], DmsError> {
+        Just([
+            SchoolEntity(
+                id: "aa-bb-pk",
+                name: "대덕소프트웨어마이스터고등학교",
+                address: "대전광역시 유성구 가정북로 76"
+            )
+        ]).setFailureType(to: DmsError.self)
+            .eraseToAnyPublisher()
+
+    }
+
+    public func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionEntity, DmsError> {
+        Just(CheckSchoolQuestionEntity(question: "")).setFailureType(to: DmsError.self)
+            .eraseToAnyPublisher()
+
+    }
+
+    public func checkSchoolQuestion(schoolID: String, answer: String) -> AnyPublisher<Void, DmsError> {
+        Just(()).setFailureType(to: DmsError.self)
+            .eraseToAnyPublisher()
+
+    }
+
+    public func checkSchoolCode(schoolID: String, code: String) -> AnyPublisher<CheckSchoolCodeEntity, DmsError> {
+        Just(CheckSchoolCodeEntity(schoolID: schoolID)).setFailureType(to: DmsError.self)
+            .eraseToAnyPublisher()
+    }
+}
