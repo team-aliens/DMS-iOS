84d94939aade3c9a2236cb13bd5030922444a3b4
Daehee Kim <0824dh@naver.com>
2022-10-20 16:16:09 +0900

    feat :: SchoolRepositoryImpl

--- Projects/Services/DataModule/Sources/School/Repositories/Impl/SchoolRepositoryImpl.swift
+++ Projects/Services/DataModule/Sources/School/Repositories/Impl/SchoolRepositoryImpl.swift
@@ -1,9 +1,29 @@
-//
-//  SchoolRepositoryImpl.swift
-//  DataModule
-//
-//  Created by 김대희 on 2022/10/20.
-//  Copyright © 2022 com.team.aliens. All rights reserved.
-//
+import Combine
+import DataMappingModule
+import DomainModule
+import ErrorModule
+import NetworkModule
 
-import Foundation
+public struct SchoolRepositoryImpl: SchoolRepository {
+    private let remoteSchoolDataSource: any RemoteSchoolDataSource
+
+    public init(remoteSchoolDataSource: any RemoteSchoolDataSource) {
+        self.remoteSchoolDataSource = remoteSchoolDataSource
+    }
+
+    public func getSchoolList() -> AnyPublisher<[SchoolEntity], DmsError> {
+        remoteSchoolDataSource.getSchoolList()
+    }
+
+    public func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionEntity, DmsError> {
+        remoteSchoolDataSource.getSchoolQuestion(authCode: authCode)
+    }
+
+    public func checkSchoolQuestion(schoolID: String, answer: String) -> AnyPublisher<Void, DmsError> {
+        remoteSchoolDataSource.checkSchoolQuestion(schoolID: schoolID, answer: answer)
+    }
+
+    public func checkSchoolCode(schoolID: String, code: String) -> AnyPublisher<CheckSchoolCodeEntity, DmsError> {
+        remoteSchoolDataSource.checkSchoolCode(schoolID: schoolID, code: code)
+    }
+}
--- Projects/Services/DomainModule/Sources/School/Repository/SchoolRepository.swift
+++ Projects/Services/DomainModule/Sources/School/Repository/SchoolRepository.swift
@@ -3,8 +3,8 @@ import DataMappingModule
 import ErrorModule
 
 public protocol SchoolRepository {
-    func getSchoolList() -> AnyPublisher<FetchSchoolListResponseDTO, DmsError>
-    func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionResponseDTO, DmsError>
+    func getSchoolList() -> AnyPublisher<[SchoolEntity], DmsError>
+    func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionEntity, DmsError>
     func checkSchoolQuestion(schoolID: String, answer: String) -> AnyPublisher<Void, DmsError>
-    func checkSchoolCode(schoolID: String, code: String) -> AnyPublisher<CheckSchoolCodeResponseDTO, DmsError>
+    func checkSchoolCode(schoolID: String, code: String) -> AnyPublisher<CheckSchoolCodeEntity, DmsError>
 }
