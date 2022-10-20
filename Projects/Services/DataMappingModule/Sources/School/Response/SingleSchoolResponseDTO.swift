b30463b7dc1783a4064209dbcdef896c1be75a9e
김대희 <0824dh@naver.com>
2022-10-20 17:35:00 +0900

    Update Projects/Services/DataMappingModule/Sources/School/Response/SingleSchoolResponseDTO.swift
    Co-authored-by: baegteun <74440939+baekteun@users.noreply.github.com>

--- Projects/Services/DataMappingModule/Sources/School/Response/SingleSchoolResponseDTO.swift
+++ Projects/Services/DataMappingModule/Sources/School/Response/SingleSchoolResponseDTO.swift
@@ -5,7 +5,7 @@ public struct SingleSchoolResponseDTO: Codable {
     public let name: String
     public let address: String
 
-    internal init(id: String, name: String, address: String) {
+    public init(id: String, name: String, address: String) {
         self.id = id
         self.name = name
         self.address = address
