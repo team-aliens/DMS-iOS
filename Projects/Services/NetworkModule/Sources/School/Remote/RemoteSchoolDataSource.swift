60b5b77d4a8d11c02d1764bd084c4be60c5d985c
Daehee Kim <0824dh@naver.com>
2022-10-20 10:50:26 +0900

    feat :: RemoteSchoolDataSource 생성

---
+++ Projects/Services/NetworkModule/Sources/School/Remote/RemoteSchoolDataSource.swift
@@ -0,0 +1,12 @@
+import APIKit
+import Combine
+import DataMappingModule
+import ErrorModule
+import Foundation
+
+public protocol RemoteSchoolDataSource {
+    func getSchoolList() -> AnyPublisher<FetchSchoolListResponseDTO, DmsError>
+    func getSchoolQuestion(authCode: String) -> AnyPublisher<CheckSchoolQuestionResponseDTO, DmsError>
+    func checkSchoolQuestion(schoolID: String, answer: String) -> AnyPublisher<Void, DmsError>
+    func checkSchoolAuthCode(schoolID: String, code: String) -> AnyPublisher<CheckSchoolAuthCodeResponseDTO, DmsError>
+}
