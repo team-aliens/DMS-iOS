214bdcdcabc901957a5cf346a22225b35ce71064
Daehee Kim <0824dh@naver.com>
2022-10-20 22:19:46 +0900

    feat :: schoolList 더미데이터 제거

--- Projects/Features/FindIDFeature/Sources/FindIDViewModel.swift
+++ Projects/Features/FindIDFeature/Sources/FindIDViewModel.swift
@@ -10,13 +10,7 @@ final class FIndIDViewModel: BaseViewModel {
     @Published var classRoom = ""
     @Published var number = ""
 
-    @Published var schoolList =  [
-        "대덕소프트웨어마이스터고등학교",
-        "대구소프트웨어마이스터고등학교",
-        "광주소프트웨어마이스터고등학교",
-        "부산소프트웨어마이스터고등학교",
-        "미림여자정보과학고"
-    ]
+    @Published var schoolList: [String] =  []
     @Published var isNavigateSignup = false
 
     private let findIDUseCase: any FindIDUseCase
