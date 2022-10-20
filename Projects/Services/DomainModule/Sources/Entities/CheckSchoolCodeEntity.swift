b321f12dd59a1a72ffdd6ee89e484ef44e1f3d71
Daehee Kim <0824dh@naver.com>
2022-10-20 13:46:03 +0900

    feat :: school API Entity

---
+++ Projects/Services/DomainModule/Sources/Entities/CheckSchoolCodeEntity.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct CheckSchoolCodeEntity: Equatable {
+
+    public let schoolID: String
+
+    public init(schoolID: String) {
+        self.schoolID = schoolID
+    }
+}
---
+++ Projects/Services/DomainModule/Sources/Entities/CheckSchoolQuestionEntity.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct CheckSchoolQuestionEntity: Equatable {
+    public let question: String
+
+    public init(question: String) {
+        self.question = question
+    }
+}
