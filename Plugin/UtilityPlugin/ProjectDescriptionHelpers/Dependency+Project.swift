284f866f364ec4bfca1b8ccc2dae50ec58cc77b4
sian7563 <sian7563@naver.com>
2022-10-21 15:59:09 +0900

    feat :: DMSApp

--- Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+Project.swift
+++ Plugin/UtilityPlugin/ProjectDescriptionHelpers/Dependency+Project.swift
@@ -10,6 +10,7 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.Project.Features {
+    static let SignupFeature = TargetDependency.feature(name: "SignupFeature")
     static let BaseFeature = TargetDependency.feature(name: "BaseFeature")
     static let RootFeature = TargetDependency.feature(name: "RootFeature")
 }
