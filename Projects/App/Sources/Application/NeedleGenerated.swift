62f9fb8d36a53c93b6a423e84889ba3c4f9afe3c
sian7563 <sian7563@naver.com>
2022-10-21 16:44:25 +0900

    feat :: NeedleGenerated

--- Projects/App/Sources/Application/NeedleGenerated.swift
+++ Projects/App/Sources/Application/NeedleGenerated.swift
@@ -5,6 +5,7 @@ import DomainModule
 import KeychainModule
 import NeedleFoundation
 import NetworkModule
+import SignupFeature
 import SwiftUI
 
 // swiftlint:disable unused_declaration
@@ -20,11 +21,52 @@ private func parent1(_ component: NeedleFoundation.Scope) -> NeedleFoundation.Sc
 
 #if !NEEDLE_DYNAMIC
 
+private class SignupDependency1ff7d1355204bb65e850Provider: SignupDependency {
+    var checkSchoolCodeUseCase: any CheckSchoolCodeUseCase {
+        return appComponent.checkSchoolCodeUseCase
+    }
+    private let appComponent: AppComponent
+    init(appComponent: AppComponent) {
+        self.appComponent = appComponent
+    }
+}
+/// ^->AppComponent->SignupComponent
+private func factory86602ff0d0dbaf2cb017f47b58f8f304c97af4d5(_ component: NeedleFoundation.Scope) -> AnyObject {
+    return SignupDependency1ff7d1355204bb65e850Provider(appComponent: parent1(component) as! AppComponent)
+}
 
 #else
 extension AppComponent: Registration {
     public func registerItems() {
 
+        localTable["keychain-any Keychain"] = { self.keychain as Any }
+        localTable["remoteAuthDataSource-any RemoteAuthDataSource"] = { self.remoteAuthDataSource as Any }
+        localTable["authRepository-any AuthRepository"] = { self.authRepository as Any }
+        localTable["signinUseCase-any SigninUseCase"] = { self.signinUseCase as Any }
+        localTable["verifyAuthCodeUseCase-any VerifyAuthCodeUseCase"] = { self.verifyAuthCodeUseCase as Any }
+        localTable["sendAuthCodeUseCase-any SendAuthCodeUseCase"] = { self.sendAuthCodeUseCase as Any }
+        localTable["checkEmailExistByAccountIDUseCase-any CheckEmailExistByAccountIDUseCase"] = { self.checkEmailExistByAccountIDUseCase as Any }
+        localTable["checkAccountIDIsExistUseCase-any CheckAccountIDIsExistUseCase"] = { self.checkAccountIDIsExistUseCase as Any }
+        localTable["signupComponent-SignupComponent"] = { self.signupComponent as Any }
+        localTable["remoteStudentsDataSource-any RemoteStudentsDataSource"] = { self.remoteStudentsDataSource as Any }
+        localTable["studentsRepository-any StudentsRepository"] = { self.studentsRepository as Any }
+        localTable["signupUseCase-any SignupUseCase"] = { self.signupUseCase as Any }
+        localTable["checkDuplicateAccountIDUseCase-any CheckDuplicateAccountIDUseCase"] = { self.checkDuplicateAccountIDUseCase as Any }
+        localTable["checkDuplicateEmailUseCase-any CheckDuplicateEmailUseCase"] = { self.checkDuplicateEmailUseCase as Any }
+        localTable["renewalPasswordUseCase-any RenewalPasswordUseCase"] = { self.renewalPasswordUseCase as Any }
+        localTable["findIDUseCase-any FindIDUseCase"] = { self.findIDUseCase as Any }
+        localTable["fetchMyProfileUseCase-any FetchMyProfileUseCase"] = { self.fetchMyProfileUseCase as Any }
+        localTable["remoteSchoolDataSource-any RemoteSchoolDataSource"] = { self.remoteSchoolDataSource as Any }
+        localTable["schoolRepository-any SchoolRepository"] = { self.schoolRepository as Any }
+        localTable["fetchSchoolListUseCase-any FetchSchoolListUseCase"] = { self.fetchSchoolListUseCase as Any }
+        localTable["fetchSchoolQuestionUseCase-any FetchSchoolQuestionUseCase"] = { self.fetchSchoolQuestionUseCase as Any }
+        localTable["checkSchoolQuestionUseCase-any CheckSchoolQuestionUseCase"] = { self.checkSchoolQuestionUseCase as Any }
+        localTable["checkSchoolCodeUseCase-any CheckSchoolCodeUseCase"] = { self.checkSchoolCodeUseCase as Any }
+    }
+}
+extension SignupComponent: Registration {
+    public func registerItems() {
+        keyPathToName[\SignupDependency.checkSchoolCodeUseCase] = "checkSchoolCodeUseCase-any CheckSchoolCodeUseCase"
     }
 }
 
@@ -44,6 +86,7 @@ private func registerProviderFactory(_ componentPath: String, _ factory: @escapi
 
 private func register1() {
     registerProviderFactory("^->AppComponent", factoryEmptyDependencyProvider)
+    registerProviderFactory("^->AppComponent->SignupComponent", factory86602ff0d0dbaf2cb017f47b58f8f304c97af4d5)
 }
 #endif
 
