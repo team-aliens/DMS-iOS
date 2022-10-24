9f8516e1f9c3f8ca518f593ffa50366188c1e324
sian7563 <sian7563@naver.com>
2022-10-24 21:46:55 +0900

    feat :: View+navigate

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Extensions/View+navigate.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+public extension View {
+    func navigate<NewView: View>(
+        to view: NewView,
+        when binding: Binding<Bool>,
+        isDetailLink: Bool = false
+    ) -> some View {
+        self.background {
+            NavigationLink(isActive: binding) {
+                DeferView {
+                    view
+                }
+            } label: {
+                EmptyView()
+            }
+            .isDetailLink(isDetailLink)
+        }
+    }
+}
+
+public struct DeferView<Content: View>: View {
+    let content: () -> Content
+
+    public init(
+        @ViewBuilder _ content: @escaping () -> Content
+    ) {
+        self.content = content
+    }
+
+    public var body: some View {
+        content()
+    }
+}
