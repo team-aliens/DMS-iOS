f080b2fcb4badd0b6a6e5e08e2213eb84f48ed60
sian7563 <sian7563@naver.com>
2022-10-25 17:00:26 +0900

    feat :: SchoolCodeViewModel 수정

---
+++ Projects/Features/SignupFeature/Sources/SchoolCodeViewModel.swift
@@ -0,0 +1,37 @@
+import BaseFeature
+import Combine
+import DomainModule
+
+final class SchoolCodeViewModel: BaseViewModel {
+
+    @Published var schoolCode = ""
+
+    @Published var isOnAutoSignup = true
+    @Published var isNavigateCheckSchool = false
+    @Published var isSuccessAuthenticationCode = false
+    @Published var isDisabled = true
+
+    var isSigninButtonEnabled: Bool {
+        !schoolCode.isEmpty
+    }
+
+    private let checkSchoolCodeUseCase: any CheckSchoolCodeUseCase
+
+    public init(checkSchoolCodeUseCase: any CheckSchoolCodeUseCase) {
+        self.checkSchoolCodeUseCase = checkSchoolCodeUseCase
+    }
+
+    func verifyAuthCodeButtonDidTap() {
+        guard isSigninButtonEnabled else { return }
+        addCancellable(
+            checkSchoolCodeUseCase.execute(
+                code: schoolCode
+            )
+        ) { [weak self] _ in
+            self?.isNavigateCheckSchool = true
+        }
+    }
+    func checkIsEmptyAuthCode() {
+        isDisabled = 8 != schoolCode.count
+    }
+}
