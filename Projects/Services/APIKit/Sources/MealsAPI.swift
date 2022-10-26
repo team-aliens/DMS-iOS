f7ee4dd83a70e996052fe1b61e5bdd097893f6ee
baegteun <baegteun@gmail.com>
2022-10-26 20:32:43 +0900

    feat :: MealsAPI:

---
+++ Projects/Services/APIKit/Sources/MealsAPI.swift
@@ -0,0 +1,39 @@
+import DataMappingModule
+import DomainModule
+import ErrorModule
+import Moya
+
+public enum MealsAPI {
+    case fetchMealList(String)
+}
+
+public extension MealsAPI: DmsAPI {
+    public var domain: DmsDomain {
+        .meals
+    }
+
+    public var urlPath: String {
+        switch self {
+        case let .fetchMealList(date):
+            return "/\(date)"
+        }
+    }
+
+    public var method: Moya.Method {
+        return .get
+    }
+
+    public var task: Moya.Task {
+        return .requestPlain
+    }
+
+    public var jwtTokenType: JwtTokenType {
+        .accessToken
+    }
+
+    public var errorMap: [Int : ErrorModule.DmsError] {
+        [
+            400: .badRequest
+        ]
+    }
+}
