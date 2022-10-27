d50809aa3bbb0692adf08a7595764518abcc55e4
sian7563 <sian7563@naver.com>
2022-10-28 01:35:54 +0900

    feat :; IDSettingView

--- Projects/Features/SignupFeature/Sources/IDSetting/IDSettingView.swift
+++ Projects/Features/SignupFeature/Sources/IDSetting/IDSettingView.swift
@@ -1,1 +1,81 @@
+import DesignSystem
+import SwiftUI
 
+struct IDSettingView: View {
+    @StateObject var viewModel: IDSettingViewModel
+
+    public init(
+        viewModel: IDSettingViewModel
+    ) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        VStack(spacing: 4) {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("DMS")
+                        .dmsFont(.title(.extraLarge), color: .PrimaryVariant.primary)
+
+                    Text("아이디 설정")
+                        .dmsFont(.text(.medium), color: .GrayScale.gray6)
+
+                    Text("학년, 반, 번호는 숫자만 입력하여 주세요.")
+                        .dmsFont(.text(.extraSmall), color: .GrayScale.gray5)
+                }
+
+                Spacer()
+            }
+            .padding(.top, 24)
+
+            VStack(spacing: 60) {
+                HStack(spacing: 20) {
+                    DMSFloatingTextField(
+                        "학년",
+                        text: $viewModel.grade,
+                        isError: viewModel.isErrorOcuured
+                    )
+
+                    DMSFloatingTextField(
+                        "반",
+                        text: $viewModel.group,
+                        isError: viewModel.isErrorOcuured
+                    )
+
+                    DMSFloatingTextField(
+                        "번호",
+                        text: $viewModel.number,
+                        isError: viewModel.isErrorOcuured
+                    )
+                }
+                .padding(.top, 94)
+
+                DMSFloatingTextField(
+                    "아이디",
+                    text: $viewModel.id,
+                    isError: viewModel.isInvalidIDError,
+                    errorMessage: "아이디가 유효하지 않습니다."
+                ) {
+                    viewModel.nextButtonDidTap()
+                }
+            }
+
+            Spacer()
+
+            .padding(.top, 60)
+
+            Spacer()
+
+            DMSWideButton(text: "다음", color: .PrimaryVariant.primary) {
+                viewModel.nextButtonDidTap()
+            }
+            .padding(.bottom, 40)
+            .disabled(!viewModel.isEnabledNextStep)
+        }
+        .dmsToast(isShowing: $viewModel.isErrorOcuured, message: viewModel.errorMessage, style: .error)
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 24)
+        .dmsBackground()
+        .ignoresSafeArea(.keyboard, edges: .bottom)
+    }
+}
