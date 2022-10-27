0179bdb8e72dff0688a8b21540d3f63c051e07c3
Daehee Kim <0824dh@naver.com>
2022-10-27 23:51:08 +0900

    feat :: ChangePasswordView

--- Projects/Features/RenewalPasswordFeature/Sources/ChangePassword/ChangePasswordView.swift
+++ Projects/Features/RenewalPasswordFeature/Sources/ChangePassword/ChangePasswordView.swift
@@ -2,6 +2,12 @@ import SwiftUI
 import DesignSystem
 
 struct ChangePasswordView: View {
+    private enum FocusField {
+        case beforePassword
+        case newPassword
+    }
+
+    @FocusState private var focusField: FocusField?
     @StateObject var viewModel: ChangePasswordViewModel
 
     public init(viewModel: ChangePasswordViewModel) {
@@ -9,6 +15,44 @@ struct ChangePasswordView: View {
     }
 
     var body: some View {
-        Text("ChangePasswordView")
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("DMS")
+                        .dmsFont(.title(.extraLarge), color: .PrimaryVariant.primary)
+                        .padding(.top, 28)
+
+                    Text("아이디 찾기")
+                        .dmsFont(.text(.medium), color: .GrayScale.gray6)
+                }
+
+                Spacer()
+            }
+
+            VStack(spacing: 60) {
+                SecureDMSFloatingTextField("새 비밀번호 입력", text: $viewModel.password) {
+                }
+                .focused($focusField, equals: .beforePassword)
+
+                SecureDMSFloatingTextField("새 비밀번호 확인 ", text: $viewModel.passwordCheck) {
+                }
+                .focused($focusField, equals: .newPassword)
+
+            }
+            .padding(.top, 68)
+
+            Spacer()
+
+            DMSWideButton(text: "확인", color: .PrimaryVariant.primary) {
+                viewModel.renewalPasswordButtonDidTap()
+            }
+            .disabled(viewModel.isRenewalPasswordButtonEnabled)
+            .padding(.bottom, 40)
+        }
+        .dmsToast(isShowing: $viewModel.isErrorOcuured, message: viewModel.errorMessage, style: .error)
+        .dmsBackground()
+        .frame(maxWidth: .infinity)
+        .ignoresSafeArea(.keyboard, edges: .bottom)
+        .padding(.horizontal, 24)
     }
 }
