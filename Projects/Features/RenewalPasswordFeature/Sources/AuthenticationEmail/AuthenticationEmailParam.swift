1a24c7034e7e35cfd01965a964f4d2c754a6a440
Daehee Kim <0824dh@naver.com>
2022-10-27 21:48:13 +0900

    feat :: AuthenticationEmailParam lint 지킴

--- Projects/Features/RenewalPasswordFeature/Sources/AuthenticationEmail/AuthenticationEmailParam.swift
+++ Projects/Features/RenewalPasswordFeature/Sources/AuthenticationEmail/AuthenticationEmailParam.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 public struct AuthenticationEmailParam: Equatable {
-    public init(name: String, email: String, id: String) {
+    public init(
+        name: String,
+        email: String,
+        id: String
+    ) {
         self.name = name
         self.email = email
         self.id = id
     }
-    
+
     public let name: String
     public let email: String
     public let id: String
