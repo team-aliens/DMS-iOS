34a0d1d2fb1ee73b265133d59714185b1106aa13
Daehee Kim <0824dh@naver.com>
2022-11-04 14:04:28 +0900

    feat :: NoticeViewModel에 들어가야 할 더미 추가

---
+++ Projects/Features/NoticeFeature/Sources/NoticeList.swift
@@ -0,0 +1,16 @@
+//  임시입니다.
+
+import Foundation
+
+struct NoticeList {
+    init(
+        title: String,
+        contentz: String
+    ) {
+        self.title = title
+        self.contentz = contentz
+    }
+
+    let title: String
+    let contentz: String
+}
--- Projects/Features/NoticeFeature/Sources/NoticeViewModel.swift
+++ Projects/Features/NoticeFeature/Sources/NoticeViewModel.swift
@@ -2,4 +2,13 @@ import BaseFeature
 import Combine
 
 final class NoticeViewModel: BaseViewModel {
+    @Published var noticeList: [NoticeList] = [
+        NoticeList(title: "11월 기숙사 봉사 관련 안내입니다", contentz: "2022/11/01"),
+        NoticeList(title: "10월 상점 제도 변경되었습니다", contentz: "2022/10/11"),
+        NoticeList(title: "지금부터 라면을 허용합니다.", contentz: "2022/10/01"),
+        NoticeList(title: "11월 기숙사 봉사 관련 안내입니다", contentz: "2022/11/01"),
+        NoticeList(title: "10월 상점 제도 변경되었습니다", contentz: "2022/10/11"),
+        NoticeList(title: "지금부터 라면을 허용합니다.", contentz: "2022/10/01"),
+        NoticeList(title: "DMS 새롭게 업데이트 될 예정입니다", contentz: "2022/09/21")
+    ]
 }
