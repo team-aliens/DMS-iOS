550f393c3745a6e5252ca2e58e71e793197d0c1a
Daehee Kim <0824dh@naver.com>
2022-11-06 23:56:44 +0900

    feat :: NoticeListViewModel

---
+++ Projects/Features/NoticeFeature/Sources/NoticeListViewModel.swift
@@ -0,0 +1,51 @@
+import BaseFeature
+import Foundation
+import DomainModule
+import ErrorModule
+import Combine
+import DataMappingModule
+
+final class NoticeListViewModel: BaseViewModel {
+    @Published var noticeList: [NoticeEntity] = []
+    @Published var noticeOrderType: NoticeOrderType = .new
+    @Published var orderTypeButtonName: String = "최신순"
+
+    private let fetchNoticeListUseCase: any FetchNoticeListUseCase
+
+    init(
+        fetchNoticeListUseCase: any FetchNoticeListUseCase
+    ) {
+        self.fetchNoticeListUseCase = fetchNoticeListUseCase
+        super.init()
+        fetchNoticeList()
+    }
+
+    func fetchNoticeList() {
+        addCancellable(
+            fetchNoticeListUseCase.execute(
+                order: noticeOrderType
+            )
+        ) { [weak self]  noticeList in
+            self?.noticeList = noticeList
+        }
+    }
+
+    func orderTypeButtonDidTap() {
+        if noticeOrderType == .new {
+            self.noticeOrderType = .old
+            self.orderTypeButtonName = "오래된순"
+            fetchNoticeList()
+        } else if noticeOrderType == .old {
+            self.noticeOrderType = .new
+            self.orderTypeButtonName = "최신순"
+            fetchNoticeList()
+        }
+    }
+
+    func dateToString(date: Date) -> String {
+        let dateFomatter = DateFormatter()
+        dateFomatter.dateFormat = "yyyy-MM-dd"
+        return dateFomatter.string(from: date)
+    }
+
+}
