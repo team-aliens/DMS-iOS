468c66f3149abcad51a6b85e7faadeda9ed4f9a9
sian7563 <sian7563@naver.com>
2022-11-10 19:43:54 +0900

    feat :: RewardPointDetailView

---
+++ Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailView.swift
@@ -0,0 +1,14 @@
+import DesignSystem
+import SwiftUI
+
+struct RewardPointDetailView: View {
+    @StateObject var viewModel: RewardPointDetailViewModel
+
+    var body: some View {
+        NavigationView {
+            VStack {
+            }
+            .navigationTitle("상벌점 현황")
+        }
+    }
+}
