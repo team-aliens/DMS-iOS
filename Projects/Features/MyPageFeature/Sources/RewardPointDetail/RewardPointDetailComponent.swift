8a222bdb85cdd41eef3ac587c780e50ea289670b
sian7563 <sian7563@naver.com>
2022-11-12 00:37:00 +0900

    feat :: RewardPointDetailComponent

--- Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailComponent.swift
+++ Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailComponent.swift
@@ -3,15 +3,12 @@ import NeedleFoundation
 import SwiftUI
 
 public protocol RewardPointDetailDependency: Dependency {
-    var fetchDetailNoticeUseCase: any FetchDetailNoticeUseCase { get }
 }
 
 public final class RewardPointDetailComponent: Component<RewardPointDetailDependency> {
     public func makeView(id: String) -> some View {
         RewardPointDetailView(
             viewModel: .init(
-                fetchDetailNoticeUseCase: self.dependency.fetchDetailNoticeUseCase,
-                id: id
             )
         )
     }
