e7f24ba2ef9df0b1446fe86362d878e6a3329032
sian7563 <sian7563@naver.com>
2022-11-12 00:38:47 +0900

    feat :: RewardPointDetailViewModel

--- Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailViewModel.swift
+++ Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailViewModel.swift
@@ -1,7 +1,17 @@
 import BaseFeature
+import UIKit
 import Combine
 import DomainModule
 
 final class RewardPointDetailViewModel: BaseViewModel {
 
+    private var fetchDetailNoticeUseCase: any FetchDetailNoticeUseCase
+
+    init(
+        fetchDetailNoticeUseCase: any FetchDetailNoticeUseCase,
+        id: String
+    ) {
+        self.fetchDetailNoticeUseCase = fetchDetailNoticeUseCase
+        super.init()
+    }
 }
