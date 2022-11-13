d324aff22122976de3f21676bd12895637107b98
sian7563 <sian7563@naver.com>
2022-11-13 21:46:24 +0900

    feat :: PointEntity 구현

---
+++ Projects/Services/DomainModule/Sources/Entities/PointEntity.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public struct PointEntity: Equatable, Hashable {
+    public init(id: String,
+                date: Date,
+                type: String,
+                name: String,
+                score: Int) {
+        self.id = id
+        self.date = date
+        self.type = type
+        self.name = name
+        self.score = score
+    }
+
+    public let id: String
+    public let date: Date
+    public let type: String
+    public let name: String
+    public let score: Int
+}
