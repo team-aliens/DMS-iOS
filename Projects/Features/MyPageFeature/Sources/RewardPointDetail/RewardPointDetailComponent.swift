2308c16bac7622bcf74a78b2c84d147e33dd1709
sian7563 <sian7563@naver.com>
2022-11-13 21:41:51 +0900

    feat :: RewardPointDetailComponent 구현

--- Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailComponent.swift
+++ Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailComponent.swift
@@ -3,13 +3,17 @@ import NeedleFoundation
 import SwiftUI
 
 public protocol RewardPointDetailDependency: Dependency {
+    var fetchMyProfileUseCase: any FetchMyProfileUseCase { get }
+    var rewardPointDetailComponent: RewardPointDetailComponent { get }
 }
 
 public final class RewardPointDetailComponent: Component<RewardPointDetailDependency> {
-    public func makeView(id: String) -> some View {
+    public func makeView() -> some View {
         RewardPointDetailView(
             viewModel: .init(
-            )
+                fetchMyProfileUseCase: self.dependency.fetchMyProfileUseCase
+            ),
+            rewardPointDetailComponent: dependency.rewardPointDetailComponent
         )
     }
 }
