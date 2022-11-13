6dba4c95f1f4e23e1f1bd851d34ce40a130b3a8e
sian7563 <sian7563@naver.com>
2022-11-13 21:42:42 +0900

    feat :: RewardPointDetailView 구현

--- Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailView.swift
+++ Projects/Features/MyPageFeature/Sources/RewardPointDetail/RewardPointDetailView.swift
@@ -5,11 +5,52 @@ struct RewardPointDetailView: View {
     @StateObject var viewModel: RewardPointDetailViewModel
     private let rewardPointDetailComponent: RewardPointDetailComponent
 
+    init(
+        viewModel: RewardPointDetailViewModel,
+        rewardPointDetailComponent: RewardPointDetailComponent
+    ) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+        self.rewardPointDetailComponent = rewardPointDetailComponent
+    }
+
     var body: some View {
         NavigationView {
             VStack {
+                ScrollView {
+                    VStack {
+                    }
+                    .padding(.horizontal, 24)
+                }
+                .navigationTitle("상벌점 현황")
+                .navigationBarTitleDisplayMode(.inline)
+                .dmsBackground()
             }
-            .navigationTitle("상벌점 현황")
         }
+    }
+
+    @ViewBuilder
+    func pointListCellView(date: String, title: String, score: String) -> some View {
+        ZStack {
+            Color.System.surface
+                .cornerRadius(6)
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(date)
+                        .dmsFont(.text(.medium), color: .System.title)
+
+                    Text(title)
+                        .dmsFont(.text(.extraSmall), color: .System.text)
+
+                    Text(score)
+                        .dmsFont(.text(.extraSmall), color: .System.text)
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+        }
+        .frame(height: 68)
+        .shadow(color: .GrayScale.gray5.opacity(0.15), blur: 20)
+
     }
 }
