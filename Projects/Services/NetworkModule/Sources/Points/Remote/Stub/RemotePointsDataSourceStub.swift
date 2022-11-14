bc9d1ad85b5537fa9436f435272a0161f7d7dc5f
sian7563 <sian7563@naver.com>
2022-11-14 17:05:31 +0900

    feat :: PointsDataSourceStub 구현

---
+++ Projects/Services/NetworkModule/Sources/Points/Remote/Stub/RemotePointsDataSourceStub.swift
@@ -0,0 +1,16 @@
+import Combine
+import DataMappingModule
+import DomainModule
+import ErrorModule
+import Foundation
+import Utility
+
+public struct RemotePointsDataSourceStub: RemotePointsDataSource {
+    public init() {}
+
+    public func fetchPointsList(type: String) -> AnyPublisher<[PointsEntity], DmsError> {
+        Just([
+        ]).setFailureType(to: DmsError.self)
+            .eraseToAnyPublisher()
+    }
+}
