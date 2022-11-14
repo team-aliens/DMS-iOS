a05461b1cadae95873aadad6a42bce1cf013fa11
sian7563 <sian7563@naver.com>
2022-11-14 21:38:23 +0900

    feat :: StudentsRepositoryStub에 profileImageURL 추가

--- Projects/Services/DataModule/Sources/Students/Repositories/Stub/StudentsRepositoryStub.swift
+++ Projects/Services/DataModule/Sources/Students/Repositories/Stub/StudentsRepositoryStub.swift
@@ -3,6 +3,7 @@ import DataMappingModule
 import DomainModule
 import ErrorModule
 import NetworkModule
+import Foundation
 
 public struct StudentsRepositoryStub: StudentsRepository {
     public init() {}
@@ -45,6 +46,10 @@ public struct StudentsRepositoryStub: StudentsRepository {
                 schoolName: "광주소프트웨어마이스터고등학교",
                 name: "변찬우",
                 gcn: "2118",
+                profileImageURL: URL(string: """
+                                     https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.nave
+r.net%2FMjAyMjExMDdfNjEg%2FMDAxNjY3Nzk5MTczMDg1.F93U6F1WhSWJbojBzO8OzLIVEmIbG24N3wSZo0ZPJc4g.pLD-fxrHiczl7lAExPrh-bqrk5GjLmwdiFLyzSM7Z1Ig.JPEG.dbdidna23%2F24.jpg&type=sc960_832
+""")!,
                 bonusPoint: 0,
                 minusPoint: 24,
                 phrase: "안녕하세요"
