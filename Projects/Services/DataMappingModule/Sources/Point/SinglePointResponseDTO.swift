20dae0f80cf4b267ddcf067748e37529a62304a8
sian7563 <sian7563@naver.com>
2022-11-14 20:29:55 +0900

    feat :: SinglePoint DTO 구현

---
+++ Projects/Services/DataMappingModule/Sources/Point/SinglePointResponseDTO.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct SinglePointResponseDTO: Decodable {
+    public init(id: String, date: Date, type: String, name: String, score: Int) {
+        self.id = id
+        self.date = date
+        self.type = type
+        self.name = name
+        self.score = score
+    }
+
+    public let id: String
+    public let date: Date
+    public let type: String
+    public let name: String
+    public let score: Int
+}
