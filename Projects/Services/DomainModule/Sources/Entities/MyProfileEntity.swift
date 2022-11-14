74d0a1f6fd71731a544f721f964f5ef080ef7f9e
sian7563 <sian7563@naver.com>
2022-11-14 12:22:41 +0900

    feat :: MyProfileEntity 바뀐 API 요소 수정

--- Projects/Services/DomainModule/Sources/Entities/MyProfileEntity.swift
+++ Projects/Services/DomainModule/Sources/Entities/MyProfileEntity.swift
@@ -4,6 +4,7 @@ public struct MyProfileEntity: Equatable {
     public let schoolName: String
     public let name: String
     public let gcn: String
+    public let profileImageURL: URL
     public let bonusPoint: Int
     public let minusPoint: Int
     public let phrase: String
@@ -11,12 +12,14 @@ public struct MyProfileEntity: Equatable {
     public init(schoolName: String,
                 name: String,
                 gcn: String,
+                profileImageURL: URL,
                 bonusPoint: Int,
                 minusPoint: Int,
                 phrase: String) {
         self.schoolName = schoolName
         self.name = name
         self.gcn = gcn
+        self.profileImageURL = profileImageURL
         self.bonusPoint = bonusPoint
         self.minusPoint = minusPoint
         self.phrase = phrase
