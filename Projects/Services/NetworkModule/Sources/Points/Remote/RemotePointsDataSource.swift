2975af1decef5e0b89db7d1c78f2e918371c29b8
sian7563 <sian7563@naver.com>
2022-11-15 10:17:04 +0900

    feat :: DataMappingModule import

--- Projects/Services/NetworkModule/Sources/Points/Remote/RemotePointsDataSource.swift
+++ Projects/Services/NetworkModule/Sources/Points/Remote/RemotePointsDataSource.swift
@@ -2,6 +2,7 @@ import Combine
 import APIKit
 import DomainModule
 import ErrorModule
+import DataMappingModule
 
 public protocol RemotePointsDataSource {
     func fetchPointsList(type: PointsType) -> AnyPublisher<[PointEntity], DmsError>
