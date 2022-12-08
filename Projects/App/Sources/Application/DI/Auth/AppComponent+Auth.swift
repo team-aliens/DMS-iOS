626a92115438a6854de6d67b127e01ed9bdcecb3
Daehee Kim <0824dh@naver.com>
2022-12-08 23:01:38 +0900

    modify :: MARK TODO로 바꾸기

--- Projects/App/Sources/Application/DI/Auth/AppComponent+Auth.swift
+++ Projects/App/Sources/Application/DI/Auth/AppComponent+Auth.swift
@@ -6,7 +6,7 @@ import DataModule
 public extension AppComponent {
     var remoteAuthDataSource: any RemoteAuthDataSource {
         shared {
-            RemoteAuthDataSourceStub() // MARK: 퍼블리싱용 Stub, 테스트 서버 나오면 변경
+            RemoteAuthDataSourceStub() // TODO: 퍼블리싱용 Stub, 테스트 서버 나오면 변경
 //            RemoteAuthDataSourceImpl(keychain: keychain)
         }
     }
