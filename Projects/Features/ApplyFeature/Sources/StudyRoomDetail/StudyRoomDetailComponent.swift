0092c14c17e5f24fb6c435360d8f4b503ea5043e
Daehee Kim <0824dh@naver.com>
2022-12-26 15:41:57 +0900

    feat :: StudyRoomSeatView

--- Projects/Features/ApplyFeature/Sources/StudyRoomDetail/StudyRoomDetailComponent.swift
+++ Projects/Features/ApplyFeature/Sources/StudyRoomDetail/StudyRoomDetailComponent.swift
@@ -1,9 +1,27 @@
-//
-//  StudyRoomDetailComponent.swift
-//  ApplyFeatureTests
-//
-//  Created by 김대희 on 2022/12/25.
-//  Copyright © 2022 com.team.aliens. All rights reserved.
-//
+import SwiftUI
+import DomainModule
+import NeedleFoundation
 
-import Foundation
+public protocol StudyRoomDetailDependency: Dependency {
+    var fetchStudyAvailableTimeUseCase: any FetchStudyAvailableTimeUseCase { get }
+    var fetchSeatTypesUseCase: any FetchSeatTypesUseCase { get }
+    var fetchDetailStudyRoomUseCase: any FetchDetailStudyRoomUseCase { get }
+    var applyStudyRoomSeatUseCase: any ApplyStudyRoomSeatUseCase { get }
+    var cancelStudyRoomSeatUseCase: any CancelStudyRoomSeatUseCase { get }
+
+}
+
+public final class StudyRoomDetailComponent: Component<StudyRoomDetailDependency> {
+    public func makeView(studyRoomID: String) -> some View {
+        StudyRoomDetailView(
+            viewModel: .init(
+                studyRoomID: studyRoomID,
+                fetchStudyAvailableTimeUseCase: dependency.fetchStudyAvailableTimeUseCase,
+                fetchSeatTypesUseCase: dependency.fetchSeatTypesUseCase,
+                fetchDetailStudyRoomUseCase: dependency.fetchDetailStudyRoomUseCase,
+                applyStudyRoomSeatUseCase: dependency.applyStudyRoomSeatUseCase,
+                cancelStudyRoomSeatUseCase: dependency.cancelStudyRoomSeatUseCase
+            )
+        )
+    }
+}
