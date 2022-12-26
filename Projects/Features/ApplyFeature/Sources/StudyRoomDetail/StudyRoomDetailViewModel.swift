9763923089042b7b6c876598235f51770ba75e04
김대희 <0824dh@naver.com>
2022-12-26 23:28:30 +0900

    feat :: apply cancel 로직 뺴고 ViewModel 완성

--- Projects/Features/ApplyFeature/Sources/StudyRoomDetail/StudyRoomDetailViewModel.swift
+++ Projects/Features/ApplyFeature/Sources/StudyRoomDetail/StudyRoomDetailViewModel.swift
@@ -1,9 +1,85 @@
-//
-//  StudyRoomDetailViewModel.swift
-//  ApplyFeatureTests
-//
-//  Created by 김대희 on 2022/12/25.
-//  Copyright © 2022 com.team.aliens. All rights reserved.
-//
+import BaseFeature
+import DomainModule
+import DataMappingModule
+import Combine
 
-import Foundation
+final class StudyRoomDetailViewModel: BaseViewModel {
+    @Published var studyRoomDetail: DetailStudyRoomEntity = .init(
+        floor: 0,
+        name: "",
+        totalAvailableSeat: 0,
+        inUseHeadcount: 0,
+        availableSex: .all,
+        availableGrade: 0,
+        eastDescription: "",
+        westDescription: "",
+        southDescription: "",
+        northDescription: "",
+        totalWidthSize: 0,
+        totalHeightSize: 0,
+        seats: []
+    )
+    @Published var seatTypes: [SeatTypeEntity] = []
+    @Published var availableTime: String = ""
+
+    var studyRoomID: String
+
+    private let fetchStudyAvailableTimeUseCase: any FetchStudyAvailableTimeUseCase
+    private let fetchSeatTypesUseCase: any FetchSeatTypesUseCase
+    private let fetchDetailStudyRoomUseCase: any FetchDetailStudyRoomUseCase
+    private let applyStudyRoomSeatUseCase: any ApplyStudyRoomSeatUseCase
+    private let cancelStudyRoomSeatUseCase: any CancelStudyRoomSeatUseCase
+
+    public init(
+        studyRoomID: String,
+        fetchStudyAvailableTimeUseCase: any FetchStudyAvailableTimeUseCase,
+        fetchSeatTypesUseCase: any FetchSeatTypesUseCase,
+        fetchDetailStudyRoomUseCase: any FetchDetailStudyRoomUseCase,
+        applyStudyRoomSeatUseCase: any ApplyStudyRoomSeatUseCase,
+        cancelStudyRoomSeatUseCase: any CancelStudyRoomSeatUseCase
+    ) {
+        self.studyRoomID = studyRoomID
+        self.fetchStudyAvailableTimeUseCase = fetchStudyAvailableTimeUseCase
+        self.fetchSeatTypesUseCase = fetchSeatTypesUseCase
+        self.fetchDetailStudyRoomUseCase = fetchDetailStudyRoomUseCase
+        self.applyStudyRoomSeatUseCase = applyStudyRoomSeatUseCase
+        self.cancelStudyRoomSeatUseCase = cancelStudyRoomSeatUseCase
+    }
+
+    func onAppear() {
+        fetchStudyAvailableTime()
+        fetchSeatType()
+        fetchDetailStudyRoom()
+    }
+
+    func fetchSeatType() {
+        addCancellable(
+            fetchSeatTypesUseCase.execute()
+        ) { [weak self] seatTypes in
+            self?.seatTypes = seatTypes
+        }
+    }
+
+    func fetchStudyAvailableTime() {
+        var startTime: String = ""
+        var endTime: String = ""
+
+        addCancellable(
+            fetchStudyAvailableTimeUseCase.execute()
+        ) { [weak self] availableTime in
+            startTime = availableTime.startAt.toSmallDMSTimeString()
+            endTime = availableTime.endAt.toSmallDMSTimeString()
+            self?.availableTime = "\(startTime) ~ \(endTime)"
+        }
+    }
+
+    func fetchDetailStudyRoom() {
+        addCancellable(
+            fetchDetailStudyRoomUseCase.execute(
+                roomID: self.studyRoomID
+            )
+        ) { [weak self] detailStudyRoom in
+            self?.studyRoomDetail = detailStudyRoom
+        }
+    }
+}
