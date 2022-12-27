2874cad0c6fac4f7c2c82b31e66ad652c0c79bc3
baegteun <baegteun@gmail.com>
2022-12-27 10:01:28 +0900

    feat :: 로그아웃 UseCase

--- Projects/Features/MyPageFeature/Sources/MyPage/MyPageComponent.swift
+++ Projects/Features/MyPageFeature/Sources/MyPage/MyPageComponent.swift
@@ -4,6 +4,7 @@ import NeedleFoundation
 
 public protocol MyPageDependency: Dependency {
     var fetchMyProfileUseCase: any FetchMyProfileUseCase { get }
+    var logoutUseCase: any LogoutUseCase { get }
     var changeProfileComponent: ChangeProfileComponent { get }
     var rewardPointDetailComponent: RewardPointDetailComponent { get }
     var checkPasswordComponent: CheckPasswordComponent { get }
--- Projects/Services/DataModule/Sources/Auth/Repositories/Impl/AuthRepositoreImpl.swift
+++ Projects/Services/DataModule/Sources/Auth/Repositories/Impl/AuthRepositoreImpl.swift
@@ -3,12 +3,22 @@ import DataMappingModule
 import DomainModule
 import ErrorModule
 import NetworkModule
+import DatabaseModule
 
 public struct AuthRepositoryImpl: AuthRepository {
     private let remoteAuthDataSource: any RemoteAuthDataSource
+    private let localAuthDataSource: any LocalAuthDataSource
 
-    public init(remoteAuthDataSource: any RemoteAuthDataSource) {
+    public init(
+        remoteAuthDataSource: any RemoteAuthDataSource,
+        localAuthDataSource: any LocalAuthDataSource
+    ) {
         self.remoteAuthDataSource = remoteAuthDataSource
+        self.localAuthDataSource = localAuthDataSource
+    }
+
+    public func logout() {
+        localAuthDataSource.logout()
     }
 
     public func signin(req: SigninRequestDTO) -> AnyPublisher<DmsFeatures, DmsError> {
