dc1b31468643caa7ff7eabc6a68fcb5c90f922b1
baegteun <baegteun@gmail.com>
2023-02-07 22:27:55 +0900

    chore :: moya link

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,7 +9,7 @@ let dependencies = Dependencies(
                 .remote(url: "https://github.com/Quick/Quick.git", requirement: .upToNextMajor(from: "5.0.1")),
                 .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0")),
                 .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0")),
-                .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.3"))
+                .remote(url: "https://github.com/team-aliens/Moya.git", requirement: .branch("master"))
             ],
             baseSettings: .settings(
                 configurations: [
