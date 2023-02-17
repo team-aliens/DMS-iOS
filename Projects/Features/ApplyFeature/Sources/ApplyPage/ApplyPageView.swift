98c67dec62bd63eb98df4e62ec543313beb9942f
InhyeKang <kanginhae0826@gmail.com>
2023-02-18 04:59:42 +0900

    refactor :: NavigationView 중첩 코드 리팩토링

--- Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageView.swift
+++ Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageView.swift
@@ -19,9 +19,9 @@ struct ApplyPageView: View {
     var body: some View {
         NavigationView {
             ScrollView(showsIndicators: false) {
-                VStack {
-//                    Spacer()
-//                        .frame(height: 20)
+                VStack(spacing: 30) {
+                    Spacer()
+                        .frame(height: 10)
                     applyListCellView(
                         viewModel: viewModel,
                         name: "자습실",
@@ -38,70 +38,29 @@ struct ApplyPageView: View {
 """,
                         buttonTitle: "잔류 신청하기",
                         state: "금요 귀가")
-                    //                    ApplyListCellView(
-                    //                        listName: "자습실",
-                    //                        listContent: """
-                    //    자습실 사용이 필요한 경우, 자습실 신청을 통해서 원하는 자리를 신청해 보세요.
-                    //    """,
-                    //                        buttonTitle: "자습실 신청하기",
-                    //                        currentState: "2층 자습실")
-                    //                    ApplyListCellView(
-                    //                        listName: "잔류",
-                    //                        listContent: """
-                    //주말 기숙사 잔류 여부를 확인하고, 잔류 신청을 통해서 잔류 또는 귀가를 신청해 보세요.
-                    //""",
-                    //                        buttonTitle: "잔류 신청하기",
-                    //                        currentState: "금요 귀가")
                 }
                 .padding(.horizontal, 24)
                 .navigationTitle("신청")
                 .navigationBarTitleDisplayMode(.inline)
                 .dmsBackground()
+                .navigate(
+                    to: studyRoomListComponent.makeView(),
+                    when: $viewModel.isNavigateToStudy
+                )
+                .navigate(
+                    to: stayApplyComponent.makeView(),
+                    when: $viewModel.isNavigateToStay
+                )
             }
         }
     }
     @ViewBuilder
-    func applyListCellView(viewModel: ApplyPageViewModel, name: String, content: String, buttonTitle: String, state: String) -> some View {
-        NavigationView {
-//            ZStack {
-//                Color.System.surface
-//                    .cornerRadius(10)
-//                VStack(alignment: .leading) {
-//                    HStack {
-//                        Text(name)
-//                            .dmsFont(.title(.title2), color: .GrayScale.gray7)
-//                            .frame(height: 32)
-//                            .padding(.vertical, 20)
-//                            .padding(.leading, 20)
-//                        Spacer()
-//                        Text(state)
-//                            .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
-//                            .frame(height: 34)
-//                            .padding(.horizontal, 14)
-//                            .background(Color.PrimaryVariant.lighten2)
-//                            .cornerRadius(24)
-//                            .padding(.trailing, 16)
-//                    }
-//                    Text(content)
-//                        .dmsFont(.body(.body3), color: .GrayScale.gray9)
-//                        .multilineTextAlignment(.leading)
-//                        .frame(alignment: .leading)
-//                        .padding(.leading, 20)
-//                        .padding(.trailing, 29.48)
-//                    DMSWideButton(
-//                        text: buttonTitle,
-//                        color: .PrimaryVariant.primary) {
-//                            if name == "자습실" {
-//                                viewModel.isNavigateToStudy.toggle()
-//                            } else {
-//                                viewModel.isNavigateToStay.toggle()
-//                            }
-//                        }
-//                        .padding(20)
-//                }
-//            }
-//            .frame(height: 206)
-//            .dmsShadow(style: .surface)
+    func applyListCellView(
+        viewModel: ApplyPageViewModel,
+        name: String,
+        content: String,
+        buttonTitle: String,
+        state: String) -> some View {
             ApplyListCellView(
                 viewModel: viewModel,
                 name: name,
@@ -109,13 +68,4 @@ struct ApplyPageView: View {
                 buttonTitle: buttonTitle,
                 state: state)
         }
-        .navigate(
-            to: studyRoomListComponent.makeView(),
-            when: $viewModel.isNavigateToStudy
-        )
-        .navigate(
-            to: stayApplyComponent.makeView(),
-            when: $viewModel.isNavigateToStay
-        )
-    }
 }
--- Projects/Features/ApplyFeature/Sources/ApplyPage/Component/ApplyListCellView.swift
+++ Projects/Features/ApplyFeature/Sources/ApplyPage/Component/ApplyListCellView.swift
@@ -2,65 +2,6 @@ import SwiftUI
 import StudyRoomFeature
 import StayApplyFeature
 import DesignSystem
-//
-//struct ApplyListCellView: View {
-//    @StateObject var viewModel: ApplyPageViewModel
-//    var listName: String
-//    var listContent: String
-//    var buttonTitle: String
-//    var currentState: String
-//    var body: some View {
-//        NavigationView {
-//            ZStack {
-//                Color.System.surface
-//                    .cornerRadius(10)
-//                VStack(alignment: .leading) {
-//                    HStack {
-//                        Text(listName)
-//                            .dmsFont(.title(.title2), color: .GrayScale.gray7)
-//                            .frame(height: 32)
-//                            .padding(.vertical, 20)
-//                            .padding(.leading, 20)
-//                        Spacer()
-//                        Text(currentState)
-//                            .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
-//                            .frame(height: 34)
-//                            .padding(.horizontal, 14)
-//                            .background(Color.PrimaryVariant.lighten2)
-//                            .cornerRadius(24)
-//                            .padding(.trailing, 16)
-//                    }
-//                    Text(listContent)
-//                        .dmsFont(.body(.body3), color: .GrayScale.gray9)
-//                        .multilineTextAlignment(.leading)
-//                        .frame(alignment: .leading)
-//                        .padding(.leading, 20)
-//                        .padding(.trailing, 29.48)
-//                    DMSWideButton(
-//                        text: buttonTitle,
-//                        color: .PrimaryVariant.primary) {
-//                            if listName == "자습실" {
-//                                viewModel.isNavigateToStudy.toggle()
-//                            } else {
-//                                viewModel.isNavigateToStay.toggle()
-//                            }
-//                        }
-//                        .padding(20)
-//                }
-//            }
-//            .frame(height: 206)
-//            .dmsShadow(style: .surface)
-//        }
-//        .navigate(
-//            to: studyRoomComponent.makeView(),
-//            when: $viewModel.isNavigateToStudy
-//        )
-//        .navigate(
-//            to: stayApplyComponent.makeView(),
-//            when: $viewModel.isNavigateToStay
-//        )
-//    }
-//}
 
 struct ApplyListCellView: View {
     @StateObject var viewModel: ApplyPageViewModel
