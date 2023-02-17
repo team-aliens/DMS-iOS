827df2d352f2d777994dd28658469ba25568f95a
InhyeKang <kanginhae0826@gmail.com>
2023-02-18 03:53:01 +0900

    feat :: 신청 메인 화면 UI 퍼블리싱

--- Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageView.swift
+++ Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageView.swift
@@ -15,10 +15,10 @@ struct ApplyPageView: View {
                     Spacer()
                         .frame(height: 20)
                     ApplyListCellView(
-                        listName: "자습실 신청",
+                        listName: "자습실",
                         listContent: """
-자습실 사용이 필요한 경우, 자습실 신청을 통해서 원하는 자리를 신청해 보세요.
-""",
+    자습실 사용이 필요한 경우, 자습실 신청을 통해서 원하는 자리를 신청해 보세요.
+    """,
                         buttonTitle: "자습실 신청하기",
                         currentState: "2층 자습실")
                     ApplyListCellView(
--- Projects/Features/ApplyFeature/Sources/ApplyPage/Component/ApplyListCellView.swift
+++ Projects/Features/ApplyFeature/Sources/ApplyPage/Component/ApplyListCellView.swift
@@ -6,36 +6,36 @@ struct ApplyListCellView: View {
     var listContent: String
     var buttonTitle: String
     var currentState: String
-
     var body: some View {
         ZStack {
             Color.System.surface
                 .cornerRadius(10)
-            HStack {
-                VStack(alignment: .leading) {
-                    ZStack {
-                        Text(listName)
-                            .dmsFont(.title(.title2), color: .GrayScale.gray7)
-                            .frame(height: 32)
-                            .padding(.vertical, 20)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(listName)
+                        .dmsFont(.title(.title2), color: .GrayScale.gray7)
+                        .frame(height: 32)
+                        .padding(.vertical, 20)
                         .padding(.leading, 20)
-                        Text(currentState)
-                            .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
-                            .background(Color.PrimaryVariant.lighten2)
-                            .frame(width: 92, height: 34)
-                            .padding(.trailing, 16)
-                    }
-                    Text(listContent)
-                        .dmsFont(.body(.body3), color: .GrayScale.gray9)
-                        .multilineTextAlignment(.leading)
-                        .frame(alignment: .leading)
-                        .padding(.leading, 20)
-                        .padding(.trailing, 29.48)
-                    DMSWideButton(
-                        text: buttonTitle,
-                        color: .PrimaryVariant.primary)
-                    .padding(20)
+                    Spacer()
+                    Text(currentState)
+                        .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
+                        .frame(height: 34)
+                        .padding(.horizontal, 14)
+                        .background(Color.PrimaryVariant.lighten2)
+                        .cornerRadius(24)
+                        .padding(.trailing, 16)
                 }
+                Text(listContent)
+                    .dmsFont(.body(.body3), color: .GrayScale.gray9)
+                    .multilineTextAlignment(.leading)
+                    .frame(alignment: .leading)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 29.48)
+                DMSWideButton(
+                    text: buttonTitle,
+                    color: .PrimaryVariant.primary)
+                .padding(20)
             }
         }
         .frame(height: 206)
