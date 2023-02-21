aa6cea13343483b7f4e0cf3d36431ba4b9a3bcad
hsj._.06 <gtw030488@gmail.com>
2023-02-21 14:13:03 +0900

    refactor :: 줄바꿈 추가

--- Projects/Services/DataMappingModule/Sources/Remains/Response/FetchListRemainApplicationItemsResponseDTO.swift
+++ Projects/Services/DataMappingModule/Sources/Remains/Response/FetchListRemainApplicationItemsResponseDTO.swift
@@ -8,6 +8,7 @@ public struct FetchListRemainAppItemsResponseDTO: Decodable {
         self.selectedOption = selectedOption
         self.remainOptions = remainOptions
     }
+    
     public let selectedOption: String
     public let remainOptions: [RemainOptionResponseDTO]
 
