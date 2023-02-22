12c888a278009b2096e530aa32443694b68caf44
hsj._.06 <gtw030488@gmail.com>
2023-02-22 14:49:32 +0900

    refactor :: 공백 제거

--- Projects/Services/NetworkModule/Sources/Remains/Remote/Impl/RemoteRemainsDataSourceImpl.swift
+++ Projects/Services/NetworkModule/Sources/Remains/Remote/Impl/RemoteRemainsDataSourceImpl.swift
@@ -9,13 +9,13 @@ public final class RemoteRemainsDataSourceImpl: BaseRemoteDataSource<RemainsAPI>
     public func remainingApplicationsChanges(id: String) -> AnyPublisher<Void, DmsError> {
         request(.remainingApplicationsChanges(id: id))
     }
-    
+
     public func fetchMyRemainApplicationItems() -> AnyPublisher<MyRemainApplicationItemsEntity, DmsError> {
         request(.fetchMyRemainApplicationItems, dto: FetchMyRemainApplicationItemsResponseDTO.self)
         .map { $0.toDomain() }
         .eraseToAnyPublisher()
     }
-    
+
     public func fetchRemainsAvailableTime() -> AnyPublisher<RemainsAvailableTimeEntity, DmsError> {
         request(.fetchRemainsAvailableTime, dto: FetchRemainsAvailableTimeResponseDTO.self)
         .map { $0.toDomain() }
