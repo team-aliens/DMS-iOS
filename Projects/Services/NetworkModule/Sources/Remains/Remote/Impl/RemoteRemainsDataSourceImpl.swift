49182e05190d6d2f3cd803acfa389275b96746a4
hsj._.06 <gtw030488@gmail.com>
2023-02-22 22:06:46 +0900

    fix :: Error solved

--- Projects/Services/NetworkModule/Sources/Remains/Remote/Impl/RemoteRemainsDataSourceImpl.swift
+++ Projects/Services/NetworkModule/Sources/Remains/Remote/Impl/RemoteRemainsDataSourceImpl.swift
@@ -9,19 +9,19 @@ public final class RemoteRemainsDataSourceImpl: BaseRemoteDataSource<RemainsAPI>
     public func remainingApplicationsChanges(id: String) -> AnyPublisher<Void, DmsError> {
         request(.remainingApplicationsChanges(id: id))
     }
-    
+
     public func fetchMyRemainApplicationItems() -> AnyPublisher<MyRemainApplicationItemsEntity, DmsError> {
         request(.fetchMyRemainApplicationItems, dto: FetchMyRemainApplicationItemsResponseDTO.self)
             .map { $0.toDomain() }
             .eraseToAnyPublisher()
     }
-    
+
     public func fetchRemainsAvailableTime() -> AnyPublisher<RemainsAvailableTimeEntity, DmsError> {
         request(.fetchRemainsAvailableTime, dto: FetchRemainsAvailableTimeResponseDTO.self)
             .map { $0.toDomain() }
             .eraseToAnyPublisher()
     }
-    
+
     public func fetchRemainApplicationList() -> AnyPublisher<RemainApplicationListEntity, DmsError> {
         request(.fetchRemainApplicationList, dto: FetchRemainAppListResponseDTO.self)
             .map { $0.toDomain() }
