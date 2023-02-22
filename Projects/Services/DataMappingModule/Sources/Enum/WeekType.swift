1b4a6061bcc40c8a342b878b85c8764450a86fcb
hsj._.06 <gtw030488@gmail.com>
2023-02-22 14:48:20 +0900

    feat :: WeekDayType

---
+++ Projects/Services/DataMappingModule/Sources/Enum/WeekType.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public enum WeekDayType: String {
+    case monDay = "MONDAY"
+    case tuesDay = "TUESDAY"
+    case wednesDay = "WEDNESDAY"
+    case thursDay = "THURSDAY"
+    case friDay = "FRIDAY"
+    case satuDay = "SATURDAY"
+    case sunDay = "SUNDAY"
+}
