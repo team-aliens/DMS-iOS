d14bee4811c78732015408ed38a1a83414132dcc
hsj._.06 <gtw030488@gmail.com>
2023-02-28 02:04:04 +0900

    fix :: 명세서 변경에 따른 수정

--- Projects/Services/DomainModule/Sources/Entities/RemainApplicationListEntity.swift
+++ Projects/Services/DomainModule/Sources/Entities/RemainApplicationListEntity.swift
@@ -2,13 +2,10 @@ import Foundation
 
 public struct RemainApplicationListEntity: Equatable, Hashable {
     public init(
-        selectedOption: String,
         remainOptions: [RemainOptionEntity]
     ) {
-        self.selectedOption = selectedOption
         self.remainOptions = remainOptions
     }
 
-    public let selectedOption: String
     public let remainOptions: [RemainOptionEntity]
 }
--- Projects/Services/DomainModule/Sources/Entities/RemainOptionEntity.swift
+++ Projects/Services/DomainModule/Sources/Entities/RemainOptionEntity.swift
@@ -4,14 +4,17 @@ public struct RemainOptionEntity: Equatable, Hashable {
     public init(
         id: String,
         title: String,
-        description: String
+        description: String,
+        isApplied: Bool
     ) {
         self.id = id
         self.title = title
         self.description = description
+        self.isApplied = isApplied
     }
 
     public let id: String
     public let title: String
     public let description: String
+    public let isApplied: Bool
 }
