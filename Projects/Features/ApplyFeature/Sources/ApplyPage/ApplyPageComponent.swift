1a622f21ad49f148bfe41a945fb58704f311be89
hsj._.06 <gtw030488@gmail.com>
2023-02-28 21:48:24 +0900

    feat :: applyPageview api 연동
    💩 Some dependencies are missing, please look at the warnings above for the list.

--- Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageComponent.swift
+++ Projects/Features/ApplyFeature/Sources/ApplyPage/ApplyPageComponent.swift
@@ -2,16 +2,21 @@ import SwiftUI
 import StudyRoomFeature
 import RemainApplyFeature
 import NeedleFoundation
+import DomainModule
 
 public protocol ApplyPageDependency: Dependency {
     var studyRoomListComponent: StudyRoomListComponent { get }
     var remainApplyComponent: RemainApplyComponent { get }
+    var fetchMyRemainApplicationItemsUseCase: any FetchMyRemainApplicationItemsUseCase { get }
+    var fetchMyStudyRoomAppItemsUseCase: any FetchMyStudyRoomAppItemsUseCase { get }
 }
 
 public final class ApplyPageComponent: Component<ApplyPageDependency> {
     public func makeView() -> some View {
         ApplyPageView(
-            viewModel: ApplyPageViewModel(),
+            viewModel: .init(
+                fetchMyRemainApplicationItemsUseCase: self.dependency.fetchMyRemainApplicationItemsUseCase,
+                fetchMyStudyRoomAppItemsUseCase: self.dependency.fetchMyStudyRoomAppItemsUseCase),
             studyRoomListComponent: dependency.studyRoomListComponent,
             remainApplyComponent: dependency.remainApplyComponent
         )
