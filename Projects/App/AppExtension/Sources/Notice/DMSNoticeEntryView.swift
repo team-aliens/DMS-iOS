3e955ca0b3e8bfd97a465eec7df6e735eead3bea
baegteun <baegteun@gmail.com>
2023-03-03 11:45:15 +0900

    :sparkles: :: Notice Medium Widget

--- Projects/App/AppExtension/Sources/Notice/DMSNoticeEntryView.swift
+++ Projects/App/AppExtension/Sources/Notice/DMSNoticeEntryView.swift
@@ -1,6 +1,7 @@
 import DesignSystem
 import DomainModule
 import SwiftUI
+import Utility
 
 struct DMSNoticeEntryView: View {
     @Environment(\.widgetFamily) var widgetFamily
@@ -20,6 +21,9 @@ struct DMSNoticeEntryView: View {
         switch widgetFamily {
         case .systemSmall:
             SmallDMSNoticeWidgetView(entry: entry)
+
+        case .systemMedium:
+            MediumDMSNoticeWidgetView(entry: entry)
             
         default:
             EmptyView()
@@ -46,5 +50,46 @@ private struct SmallDMSNoticeWidgetView: View {
                 .dmsFont(.etc(.overline), color: .PrimaryVariant.darken1)
         }
         .padding(10)
+    }
+}
+
+private struct MediumDMSNoticeWidgetView: View {
+    @Environment(\.colorScheme) var colorScheme
+    let entry: DMSNoticeProvider.Entry
+
+    var body: some View {
+        VStack(spacing: 12) {
+            HStack(spacing: 8) {
+                Image(systemName: "megaphone")
+                    .resizable()
+                    .frame(width: 13, height: 12)
+                    .foregroundColor(.PrimaryVariant.primary)
+
+                Text("공지")
+                    .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
+
+                Spacer()
+
+                let createdAt = entry.notice.createdAt
+                let createdAtString = "\(createdAt.year)/\(createdAt.month)/\(createdAt.day)"
+                Text(createdAtString)
+                    .dmsFont(.etc(.caption), color: .GrayScale.gray5)
+            }
+
+            VStack {
+                Text(entry.notice.title)
+                    .lineSpacing(12)
+                    .dmsFont(.etc(.caption), color: colorScheme == .light ? .GrayScale.gray6 : .GrayScale.gray7)
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                RoundedRectangle(cornerRadius: 18)
+                    .fill(Color.GrayScale.gray1)
+                    .dmsShadow(style: .surface)
+            }
+        }
+        .padding(16)
     }
 }
