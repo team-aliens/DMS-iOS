7654c3c694f32105d56f8da0f94e270d8ab69682
baegteun <baegteun@gmail.com>
2023-03-03 14:21:34 +0900

    :sparkles: :: Small 상벌점 위젯

--- Projects/App/AppExtension/Sources/Point/DMSPointEntryView.swift
+++ Projects/App/AppExtension/Sources/Point/DMSPointEntryView.swift
@@ -34,45 +34,82 @@ private struct SmallDMSPointWidgetView: View {
 
     var body: some View {
         VStack(spacing: 10) {
-            Group {
-                HStack(alignment: .center) {
-                    VStack {
-                        Text("상점")
-                            .dmsFont(.etc(.caption), color: .PrimaryVariant.primary)
-
-                        Spacer()
-                    }
-
-                    Spacer()
-
-                    Text("\(entry.bonusPoint)")
-                        .dmsFont(.headline(.headline3), color: .PrimaryVariant.primary)
-                }
-                .padding(.top, 10)
-
-                HStack(alignment: .center) {
-                    VStack {
-                        Text("벌점")
-                            .dmsFont(.etc(.caption), color: .ErrorVariant.darken1)
-
-                        Spacer()
-                    }
-
-                    Spacer()
-
-                    Text("\(entry.minusPoint)")
-                        .dmsFont(.headline(.headline3), color: .ErrorVariant.darken1)
-                }
-                .padding(.top, 10)
-            }
-            .padding(.horizontal, 16)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background {
-                RoundedRectangle(cornerRadius: 18)
-                    .fill(Color.GrayScale.gray1)
-                    .dmsShadow(style: .surface)
+            if entry.displayPointPart == .both {
+                bothView()
+            } else {
+                pointView()
             }
         }
         .padding(12)
+    }
+
+    @ViewBuilder
+    func bothView() -> some View {
+        Group {
+            HStack(alignment: .center) {
+                VStack {
+                    Text("상점")
+                        .dmsFont(.etc(.caption), color: .PrimaryVariant.primary)
+
+                    Spacer()
+                }
+                .padding(.top, 10)
+
+                Spacer()
+
+                Text("\(entry.bonusPoint)")
+                    .dmsFont(.headline(.headline3), color: .PrimaryVariant.primary)
+            }
+
+            HStack(alignment: .center) {
+                VStack {
+                    Text("벌점")
+                        .dmsFont(.etc(.caption), color: .ErrorVariant.darken1)
+
+                    Spacer()
+                }
+                .padding(.top, 10)
+
+                Spacer()
+
+                Text("\(entry.minusPoint)")
+                    .dmsFont(.headline(.headline3), color: .ErrorVariant.darken1)
+            }
+        }
+        .padding(.horizontal, 16)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            RoundedRectangle(cornerRadius: 18)
+                .fill(Color.GrayScale.gray1)
+                .dmsShadow(style: .surface)
+        }
+    }
+
+    @ViewBuilder
+    func pointView() -> some View {
+        HStack {
+            VStack {
+                Text(entry.displayPointPart.display)
+                    .dmsFont(.body(.body2), color: entry.displayPointPart.foreground)
+
+                Spacer()
+            }
+
+            Spacer()
+
+            VStack {
+                Spacer()
+
+                Text(
+                    entry.displayPointPart == .bonus
+                    ? "\(entry.bonusPoint)"
+                    : "\(entry.minusPoint)"
+                )
+                .dmsFont(.headline(.headline2), color: entry.displayPointPart.foreground)
+            }
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 16)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
