2d6100c6c27785d8df1dcfb8663b2328a3d72853
baegteun <baegteun@gmail.com>
2023-03-04 19:28:42 +0900

    :sparkles: :: Notice Factory

---
+++ Projects/WatchApp/Sources/Presentation/Notice/Factory/NoticeFactory.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import WatchRestAPIModule
+
+struct NoticeFactory {
+    private let noticeRepository: any NoticeRepository
+
+    init(noticeRepository: any NoticeRepository){
+        self.noticeRepository = noticeRepository
+    }
+
+    func makeView() -> some View {
+        let model = NoticeModel()
+        let intent = NoticeIntent(model: model, noticeRepository: noticeRepository)
+        let container = MVIContainer(
+            intent: intent as NoticeIntentProtocol,
+            model: model as NoticeStateProtocol,
+            modelChangePublisher: model.objectWillChange
+        )
+        return NoticeView(container: container)
+    }
+}
