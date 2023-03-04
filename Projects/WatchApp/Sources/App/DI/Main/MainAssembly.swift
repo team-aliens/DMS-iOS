c73239e7fe50973a85d8430dffe7377248892e74
baegteun <baegteun@gmail.com>
2023-03-04 19:45:47 +0900

    :bug: :: MainFactory di regist with noticeFactory

--- Projects/WatchApp/Sources/App/DI/Main/MainAssembly.swift
+++ Projects/WatchApp/Sources/App/DI/Main/MainAssembly.swift
@@ -3,7 +3,10 @@ import Swinject
 struct MainAssembly: Assembly {
     func assemble(container: Container) {
         container.register(MainFactory.self) { resolver in
-            MainFactory(mealFactory: resolver.resolve(MealFactory.self)!)
+            MainFactory(
+                mealFactory: resolver.resolve(MealFactory.self)!,
+                noticeFactory: resolver.resolve(NoticeFactory.self)!
+            )
         }
     }
 }
