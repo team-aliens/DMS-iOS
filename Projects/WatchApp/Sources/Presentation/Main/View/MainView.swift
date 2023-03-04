feaf4dcbbefa5fc3a099db3ec900d782b2581d08
baegteun <baegteun@gmail.com>
2023-03-04 19:29:18 +0900

    :sparkles: :: Tab in notice view

--- Projects/WatchApp/Sources/Presentation/Main/View/MainView.swift
+++ Projects/WatchApp/Sources/Presentation/Main/View/MainView.swift
@@ -3,15 +3,18 @@ import SwiftUI
 struct MainView: View {
     @StateObject var container: MVIContainer<MainIntentProtocol, MainStateProtocol>
     private let mealFactory: MealFactory
+    private let noticeFactory: NoticeFactory
     private var intent: any MainIntentProtocol { container.intent }
     private var state: any MainStateProtocol { container.model }
 
     init(
         container: MVIContainer<MainIntentProtocol, MainStateProtocol>,
-        mealFactory: MealFactory
+        mealFactory: MealFactory,
+        noticeFactory: NoticeFactory
     ) {
         _container = StateObject(wrappedValue: container)
         self.mealFactory = mealFactory
+        self.noticeFactory = noticeFactory
     }
 
     var body: some View {
@@ -23,7 +26,7 @@ struct MainView: View {
                 mealFactory.makeView()
                     .tag(MainTab.meal)
 
-                Text("Notice")
+                noticeFactory.makeView()
                     .tag(MainTab.notice)
             }
             .tabViewStyle(.page)
