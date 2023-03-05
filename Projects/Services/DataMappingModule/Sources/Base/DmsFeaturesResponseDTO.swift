01035826b41080f59a4e306c72c34c4e993f1833
InhyeKang <kanginhae0826@gmail.com>
2023-03-05 14:36:58 +0900

    feat :: DmsFeaturesResponseDTO에 remainService 추가

--- Projects/Services/DataMappingModule/Sources/Base/DmsFeaturesResponseDTO.swift
+++ Projects/Services/DataMappingModule/Sources/Base/DmsFeaturesResponseDTO.swift
@@ -6,8 +6,10 @@ public struct DmsFeaturesResponseDTO: Decodable {
 
 public struct DmsFeaturesDTO: Decodable {
     public let studyRoomService: Bool
+    public let remainService: Bool
 
     enum CodingKeys: String, CodingKey {
         case studyRoomService = "study_room_service"
+        case remainService = "remain_service"
     }
 }
