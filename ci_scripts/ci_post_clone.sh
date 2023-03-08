4902b6025bb11f75debcce32422703e6b1411ae2
baegteun <baegteun@gmail.com>
2023-03-08 12:22:50 +0900

    fix :: CI를 할때 xcconfig를 구성합니다ㅏ

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,5 +2,7 @@
 sh ci_install_tuist.sh
 cd ../
 git clone https://github.com/team-aliens/DMS-XCConfig.git
+mv DMS-XCConfig/XCConfig/ .
+
 ci_scripts/tuist/tuist fetch
 TUIST_CI=1 ci_scripts/tuist/tuist generate
