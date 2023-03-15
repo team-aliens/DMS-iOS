244931680f7eced54d8d295088bc05d39d1233c0
InhyeKang <kanginhae0826@gmail.com>
2023-03-15 23:24:07 +0900

    refactor :: 쓸 데 없는 코드 삭제

--- Projects/Features/StudyRoomFeature/Sources/StudyroomApplication/StudyroomTimeListView.swift
+++ Projects/Features/StudyRoomFeature/Sources/StudyroomApplication/StudyroomTimeListView.swift
@@ -3,7 +3,6 @@ import DesignSystem
 import SwiftUI
 
 struct StudyroomTimeListView: View {
-    @State var isAvailable: Bool = true
     @State var isClicked: Bool = false
     var body: some View {
         VStack(alignment: .leading, spacing: 37) {
@@ -13,40 +12,38 @@ struct StudyroomTimeListView: View {
                 .padding(.top, 40)
                 .padding(.leading, 24)
 
-            if isAvailable {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(0..<5) { _ in
-                            Button {
-                                isClicked.toggle()
-                            } label: {
-                                Text("10시 ~ 11시")
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 14)
-                                    .dmsFont(
-                                        .etc(.button),
-                                        color: isClicked ? .GrayScale.gray1 : .GrayScale.gray4
-                                    )
-                                    .background {
-                                        isClicked ? Color.PrimaryVariant.primary : Color.GrayScale.gray1
-                                    }
-                            }
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .inset(by: 1)
-                                    .stroke(
-                                        isClicked ? Color.System.primary : Color.GrayScale.gray4,
-                                        lineWidth: 2
-                                    )
-                            )
-                            .cornerRadius(5)
-                            .padding(.horizontal, 5)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<5) { _ in
+                        Button {
+                            isClicked.toggle()
+                        } label: {
+                            Text("10시 ~ 11시")
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 14)
+                                .dmsFont(
+                                    .etc(.button),
+                                    color: isClicked ? .GrayScale.gray1 : .GrayScale.gray4
+                                )
+                                .background {
+                                    isClicked ? Color.PrimaryVariant.primary : Color.GrayScale.gray1
+                                }
                         }
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .inset(by: 1)
+                                .stroke(
+                                    isClicked ? Color.System.primary : Color.GrayScale.gray4,
+                                    lineWidth: 2
+                                )
+                        )
+                        .cornerRadius(5)
+                        .padding(.horizontal, 5)
                     }
                 }
-                .padding(.horizontal, 19)
-                .padding(.bottom, 10)
             }
+            .padding(.horizontal, 19)
+            .padding(.bottom, 10)
 
             DMSWideButton(
                 text: "확인",
@@ -58,13 +55,6 @@ struct StudyroomTimeListView: View {
             )
             .padding(.horizontal, 24)
             .padding(.bottom)
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
-                withAnimation {
-                    isAvailable = true
-                }
-            }
         }
     }
 }
