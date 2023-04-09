cee177ff064ac0f17504c25bc26268c6aee48f75
InhyeKang <kanginhae0826@gmail.com>
2023-04-09 13:57:52 +0900

    fix :: 자습실 신청 시 이름 미표시 오류 해결
    SeatStatus 처리 변경

--- Projects/Features/StudyRoomFeature/Sources/StudyRoomDetail/StudyRoomDetailView.swift
+++ Projects/Features/StudyRoomFeature/Sources/StudyRoomDetail/StudyRoomDetailView.swift
@@ -57,6 +57,7 @@ struct StudyRoomDetailView: View {
                 DMSWideButton(text: "취소", style: .contained, color: .GrayScale.gray6) {
                     viewModel.cancelStudyRoomSeat()
                 }
+//                .disabled(viewModel.selectedSeat == nil)
 
                 DMSWideButton(text: "신청", style: .contained, color: .System.primary) {
                     viewModel.applyStudyRoomSeat()
