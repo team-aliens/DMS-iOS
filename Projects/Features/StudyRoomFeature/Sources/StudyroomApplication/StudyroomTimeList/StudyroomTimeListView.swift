813e2bfe96f7b74832d33410f2604bece8409bea
InhyeKang <kanginhae0826@gmail.com>
2023-04-11 19:00:04 +0900

    feat :: 자습실 이용 시간 Bottom Sheet에 dmsBackground 설정

--- Projects/Features/StudyRoomFeature/Sources/StudyroomApplication/StudyroomTimeList/StudyroomTimeListView.swift
+++ Projects/Features/StudyRoomFeature/Sources/StudyroomApplication/StudyroomTimeList/StudyroomTimeListView.swift
@@ -37,6 +37,7 @@ struct StudyroomTimeListView: View {
             .padding(.horizontal, 24)
             .padding(.bottom)
         }
+        .dmsBackground()
         .onAppear {
             viewModel.fetchStudyroomTimeList()
         }
