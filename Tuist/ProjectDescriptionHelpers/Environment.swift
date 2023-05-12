b7ff0448795a8b81a7290fbc2f0587f430b5ca3b
InhyeKang <kanginhae0826@gmail.com>
2023-05-12 23:10:23 +0900

    refactor :: Environment 설정

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -1,11 +1,31 @@
 import ProjectDescription
 
-public enum Environment {
-    public static let appName = "DMS-Aliens"
-    public static let targetName = "DMS-Aliens"
-    public static let targetTestName = "\(targetName)Tests"
-    public static let organizationName = "com.team.aliens"
-    public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone, .ipad])
-    public static let platform = Platform.iOS
-    public static let baseSetting: SettingsDictionary = SettingsDictionary()
+//public enum Environment {
+//    public static let appName = "DMS-Aliens"
+//    public static let targetName = "DMS-Aliens"
+//    public static let targetTestName = "\(targetName)Tests"
+//    public static let organizationName = "com.team.aliens"
+//    public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "15.0", devices: [.iphone, .ipad])
+//    public static let platform = Platform.iOS
+//    public static let baseSetting: SettingsDictionary = SettingsDictionary()
+//}
+
+public struct ProjectEnvironment {
+    public let appName: String
+    public let targetName: String
+    public let targetTestName: String
+    public let organizationName: String
+    public let deploymentTarget: DeploymentTarget
+    public let platform: Platform
+    public let baseSetting: SettingsDictionary
 }
+
+public let env = ProjectEnvironment (
+    appName: "DMS-Aliens",
+    targetName: "DMS-Aliens",
+    targetTestName: "DMS-AliensTests",
+    organizationName: "com.team.aliens",
+    deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone, .ipad]),
+    platform: .iOS,
+    baseSetting: [:]
+)
