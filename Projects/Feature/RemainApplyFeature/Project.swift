10916d47a6bb08323179d9494e1ef446f53ae63e
InhyeKang <kanginhae0826@gmail.com>
2023-05-13 13:42:20 +0900

    refactor :: SwiftLint 공백 설정

--- Projects/Feature/RemainApplyFeature/Project.swift
+++ Projects/Feature/RemainApplyFeature/Project.swift
@@ -1,12 +1,13 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-//let project = Project.makeModule(
-//    name: "RemainApplyFeature",
-//    product: .staticFramework,
-//    dependencies: [
-//        .Feature.BaseFeature
-//    ])
+// let project = Project.makeModule(
+//     name: "RemainApplyFeature",
+//     product: .staticFramework,
+//     dependencies: [
+//         .Feature.BaseFeature
+//     ]
+// )
 let project = Project.makeModule(
     name: "RemainApplyFeature",
     product: .staticLibrary,
