ac2bead7d9f69fe4603cb1aae2ccdacdf19b1a8f
hsj._.06 <gtw030488@gmail.com>
2023-06-12 11:44:55 +0900

    fix :: param 수정

--- Projects/Feature/HomeFeature/Sources/HomeView.swift
+++ Projects/Feature/HomeFeature/Sources/HomeView.swift
@@ -62,6 +62,7 @@ struct HomeView: View {
 
                     MealCarouselView(
                         meal: viewModel.selectedDateMeal,
+                        selectedDate: viewModel.selectedDate,
                         isLoading: $viewModel.isLoading
                     )
                     .padding(.top, 36)
