d0f39642137bfbf7f65b269254aa90b99630301b
InhyeKang <kanginhae0826@gmail.com>
2024-03-13 22:44:29 +0900

    ci :: path ..

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
 curl -Ls https://install.tuist.io | bash
 
 cd ../
 git clone https://github.com/team-aliens/DMS-XCConfig.git
 mv DMS-XCConfig/XCConfig/ .
 
-echo "tuist generate..."
 tuist clean --path ..
 tuist fetch --path ..
-TUIST_CI=1 tuist generate --path ,,
+TUIST_CI=1 tuist generate --path ..
