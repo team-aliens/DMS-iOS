0c92802497afe59cf43ee3fe90511807f0f57ec9
InhyeKang <kanginhae0826@gmail.com>
2024-03-13 20:14:23 +0900

    ci :: chmod

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-curl https://mise.jdx.dev/install.sh | sh
-curl https://mise.run | sh
-mise install tuist
-eval "$(~/.local/bin/mise activate zsh)"
+chmod +x ci_install_tuist.sh
+./ci_install_tuist.sh
 
 cd ../
 git clone https://github.com/team-aliens/DMS-XCConfig.git
