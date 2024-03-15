018475a78dc0c533a859cec01743f8b529cddbb1
InhyeKang <kanginhae0826@gmail.com>
2024-03-15 09:41:22 +0900

    ci :: curl -ls in ci_install_tuist

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-chmod +x ci_post_clone.sh
-./ci_post_clone.sh
-chmod +x ci_install_tuist.sh
-./ci_install_tuist.sh
 
 cd ../
 git clone https://github.com/team-aliens/DMS-XCConfig.git
 mv DMS-XCConfig/XCConfig/ .
 
-ci_scripts/tuist/tuist fetch
-TUIST_CI=1 ci_scripts/tuist/tuist generate
+chmod +x ci_install_tuist.sh
+./ci_install_tuist.sh
